2f7ff33f00dfa2fc8b712e94460217889d47ea42
eyousefifar <eyousefifar@gmail.com>
2021-08-20 21:47:21 +0430

    themes constants

---
+++ app/library/theme/index.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  primaryPurple: "#7B1FA2",
+  lightPurple: "#ae52d4",
+  DarkPurple: "#4a0072",
+  transparentPurple: "rgba(114,0,202, 0.2)",
+  transparentGrey: "rgba(112,112,112,0.3)",
+  transparentWhite: "rgba(255,255,255, 0.7)",
+  lightGrey: "#afafaf",
+  grey: "#808080",
+  darkGrey: "#545454",
+};
+
+export const fonts = {
+  regular: "",
+  medium: "",
+  light: "",
+  thin: "",
+};
