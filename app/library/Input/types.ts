10bb663fedeb46c887f23a1f9ee939747a21fd4b
eyousefifar <eyousefifar@gmail.com>
2021-08-22 20:00:26 +0430

    added validate function native event function for auto grow

--- app/library/Input/types.ts
+++ app/library/Input/types.ts
@@ -1,8 +1,12 @@
-import { TextInputProps } from "react-native";
+import { NativeSyntheticEvent, TextInputContentSizeChangeEventData, TextInputProps } from "react-native";
 
 
 
 export interface IInputProps extends Omit<TextInputProps, "ref" | "onBlur"> {
     title: string
-    mode: "outlined" | "flat" 
+    mode: "outlined" | "flat";
+    validate?: (text: string) => string;
 }
+
+
+export type tOnContentSize = NativeSyntheticEvent<TextInputContentSizeChangeEventData>
