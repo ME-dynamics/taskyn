d22f81432254632a4fae4dd17dabb45e1bfa3817
eyousefifar <eyousefifar@gmail.com>
2021-08-22 20:48:52 +0430

    timer types

--- app/library/Input/types.ts
+++ app/library/Input/types.ts
@@ -1,12 +1,16 @@
-import { NativeSyntheticEvent, TextInputContentSizeChangeEventData, TextInputProps } from "react-native";
+import {
+  NativeSyntheticEvent,
+  TextInputContentSizeChangeEventData,
+  TextInputProps,
+} from "react-native";
 
-
-
+import { ITime } from "../Timer/types";
 export interface IInputProps extends Omit<TextInputProps, "ref" | "onBlur"> {
-    title: string
-    mode: "outlined" | "flat";
-    validate?: (text: string) => string;
+  title: string;
+  mode: "outlined" | "flat";
+  validate?: (text: string) => string;
+  timer?: ITime;
 }
 
-
-export type tOnContentSize = NativeSyntheticEvent<TextInputContentSizeChangeEventData>
+export type tOnContentSize =
+  NativeSyntheticEvent<TextInputContentSizeChangeEventData>;
