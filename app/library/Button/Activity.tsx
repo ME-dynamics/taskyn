b2ef9a3f28b9da02334f6085122167fa521f2950
eyousefifar <eyousefifar@gmail.com>
2021-08-22 14:31:31 +0430

    button activity

---
+++ app/library/Button/Activity.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import { observer } from 'mobx-react-lite';
+
+import { activityStyleGen } from "./styles"
+
+// types
+import { IActivityProps } from "./types"
+
+export const Activity = observer((props: IActivityProps) => {
+  const { mode } = props;
+  const { activityColor, styles } = activityStyleGen(mode);
+  return (
+    <ActivityIndicator
+      style={styles.container}
+      size={'small'}
+      color={activityColor}
+    />
+  );
+})
+
