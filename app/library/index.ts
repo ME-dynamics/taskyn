7a81335434c8f1b31a40630b1816555e4c1ce428
eyousefifar <eyousefifar@gmail.com>
2021-08-24 16:46:51 +0430

    export search bar

--- app/library/index.ts
+++ app/library/index.ts
@@ -2,5 +2,5 @@ export { Input } from "./Input";
 export { Button } from "./Button";
 export { colors } from "./theme";
 export { Touchable } from "./Touchable";
-
 export { Caption, Headline, Paragraph, Subheading, Title } from "./Typography";
+export { SearchBar } from "./SearchBar";
