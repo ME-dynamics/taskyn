ef6e4107260d9bc5ae7d0c64f2d31245b552a1d3
sssajjad007 <sssajjad.76@gmail.com>
2021-08-24 11:52:58 +0430

    fix image type and change patientname to name

--- app/patientList/screens/index.tsx
+++ app/patientList/screens/index.tsx
@@ -8,7 +8,7 @@ export default function index() {
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
     <Search/>
       <PatientCard
-        patientName="سجاد سیف اله"
+        name="سجاد سیف اله"
         onPress={() => "hi"}
         image="https://bootdey.com/img/Content/avatar/avatar7.png"
         description="افسردگی ، مشکل اعصاب ، 25 جلسه ، تست ibtm افسردگی ، مشکل اعصاب ، 25 جلسه ، تست ibtm"
