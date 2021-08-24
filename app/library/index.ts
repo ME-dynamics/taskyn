9d5eeba07d6bd7ff91f418ec259b525e85f24146
eyousefifar <eyousefifar@gmail.com>
2021-08-24 16:00:12 +0430

    exprot typography

--- app/library/index.ts
+++ app/library/index.ts
@@ -1,4 +1,6 @@
 export { Input } from "./Input";
 export { Button } from "./Button";
 export { colors } from "./theme";
-export {Touchable} from './Touchable'
+export { Touchable } from "./Touchable";
+
+export { Caption, Headline, Paragraph, Subheading, Title } from "./Typography";
