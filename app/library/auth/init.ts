40cc51033e3ce7f0b68a4320b549655a852f4852
eyousefifar <eyousefifar@gmail.com>
2021-08-30 17:33:30 +0430

    init authentication

---
+++ app/library/auth/init.ts
@@ -0,0 +1,19 @@
+import { secureStorage, storage } from "../storage";
+import { auth } from "./state";
+export async function init() {
+  const [token, refreshToken, tokenExpiresAt, refreshExpiresAt] =
+    await Promise.all([
+      secureStorage.retrieve("refresh_token"),
+      secureStorage.retrieve("token"),
+      storage.retrieve("token_expires_at"),
+      storage.retrieve("refresh_expires_at"),
+    ]);
+  auth.setRefreshToken(refreshToken);
+  auth.setToken(token);
+  auth.setTokenExpire(
+    tokenExpiresAt ? parseInt(tokenExpiresAt, 10) : undefined
+  );
+  auth.setRefreshExpire(
+    refreshExpiresAt ? parseInt(refreshExpiresAt, 10) : undefined
+  );
+}
