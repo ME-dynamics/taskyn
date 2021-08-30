a01c45d392fe2607bb5ce7fcf94e2edf6babc79e
eyousefifar <eyousefifar@gmail.com>
2021-08-30 17:33:42 +0430

    silent refresh

---
+++ app/library/auth/silentRefresh.ts
@@ -0,0 +1,50 @@
+import { autorun } from "mobx";
+import { AppState } from "react-native"
+import { auth } from "./state"
+
+let tokenTimer: NodeJS.Timer; 
+
+let refreshTimer: NodeJS.Timer;
+
+function silentRenewToken() {
+    if(auth.tokenExpiresAt) {
+        if(auth.tokenExpiresAt < Date.now()) {
+            // refresh
+        } else {
+            // time to refresh the token is : time token expires minus now minus two seconds
+            // refresh request is going to be done two seconds before token expires 
+            const timeToRefresh =  auth.tokenExpiresAt - Date.now() - 2000; 
+            tokenTimer = setTimeout(() => {
+                // refresh token
+            }, timeToRefresh)
+        }
+        
+    }
+    
+}
+
+function silentRenewRefreshToken() {
+    if(auth.refreshExpiresAt) {
+        if(auth.refreshExpiresAt < Date.now()) {
+            // refresh
+        } else {
+             // time to refresh the token is : time token expires minus now minus two seconds
+            // refresh request is going to be done two seconds before token expires
+            const timeToRefresh =  auth.refreshExpiresAt - Date.now() - 2000; 
+            refreshTimer = setTimeout(() => {
+                // refresh token
+            }, timeToRefresh)
+
+        }
+    }
+}
+const tokenDisposer = autorun(silentRenewToken);
+const refreshDisposer = autorun(silentRenewRefreshToken);
+
+
+AppState.addEventListener("change", function changeState(state) {
+    if(state === "background") {
+        clearTimeout(tokenTimer);
+        clearTimeout(refreshTimer);
+    }
+})
