9082210a0e7ceb0169c2ba640fe8784e15ba8b23
sssajjad007 <sssajjad.76@gmail.com>
2021-09-04 12:23:13 +0430

    create screen for formlist ( need test and recyleview)

---
+++ app/formList/screens/index.tsx
@@ -0,0 +1,34 @@
+//@ts-nocheck
+import React from "react";
+import { View, Text, } from "react-native";
+import { styles } from "./style";
+import { FormCard } from "../components";
+import { MaterialCommunityIcons, FontAwesome5 } from "@expo/vector-icons";
+import { SearchBar } from "../../library";
+export const FormList = () => {
+  return (
+ 
+    <View style={styles.container}>
+      <View style={styles.searchBarContainer}>
+        <SearchBar
+          onChangeText={() => {}}
+          onClear={() => {}}
+          value={""}
+          placeholder=""
+        />
+      </View>
+
+      <View style={styles.formCardContainer}>
+        <FormCard
+          Icon={(color, size) => (
+            <FontAwesome5 name="neos" size={size} color={color} />
+          )}
+          id={"Neo"}
+          englishName={"NEO Personality Inventory"}
+          persianName={"پرسش نامه شخصیتی نئو"}
+          onPress={() => {}}
+        />
+      </View>
+    </View>
+  );
+};
