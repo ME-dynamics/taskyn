1b6010c54bf12b52a4d7b39898c18c24b643689d
eyousefifar <eyousefifar@gmail.com>
2021-09-07 13:20:26 +0430

    move Icons to separate render function

--- app/profile/interfaces/screens/index.tsx
+++ app/profile/interfaces/screens/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { View, Image } from "react-native";
-import { MenuItem } from "../components/menuItem";
-import { MaterialCommunityIcons, Octicons,FontAwesome5   } from "@expo/vector-icons";
+import { View, Image, Alert } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Shadow } from "react-native-shadow-2";
-import { Alert } from "react-native";
+
+
+import { MenuItem } from "../components/MenuItem";
 import { styles } from "./style";
+import { IIconProps } from "./type"
 
 export const Profile = () => {
+  const renderAccountCircle = (props: IIconProps) => {
+    const { color, size } = props;
+    return (
+      <MaterialCommunityIcons
+        name={"account-circle"}
+        size={size}
+        color={color}
+      />
+    );
+  };
   return (
     <View style={styles.container}>
       <Image
@@ -17,46 +29,22 @@ export const Profile = () => {
       <Shadow distance={10} viewStyle={styles.bodyShadow}>
         <View style={styles.body}>
           <MenuItem
-            Icon={(color, size) => (
-              <MaterialCommunityIcons
-                name="account-circle"
-                size={size}
-                color={color}
-              />
-            )}
+            Icon={renderAccountCircle}
             title="مشخصات شخصی"
             onPress={() => Alert.alert("hi1")}
           />
-           <MenuItem
-            Icon={(color, size) => (
-              <FontAwesome5
-                name="tasks"
-                size={size}
-                color={color}
-              />
-            )}
+          <MenuItem
+            Icon={renderAccountCircle}
             title="تمرین"
             onPress={() => Alert.alert("hi1")}
           />
-           <MenuItem
-            Icon={(color, size) => (
-              <Octicons 
-                name="checklist"
-                size={size}
-                color={color}
-              />
-            )}
+          <MenuItem
+            Icon={renderAccountCircle}
             title="فرم ها"
             onPress={() => Alert.alert("hi1")}
           />
-           <MenuItem
-            Icon={(color, size) => (
-              <MaterialCommunityIcons
-                name="clipboard-notes"
-                size={size}
-                color={color}
-              />
-            )}
+          <MenuItem
+            Icon={renderAccountCircle}
             title="یادداشت"
             onPress={() => Alert.alert("hi1")}
           />
