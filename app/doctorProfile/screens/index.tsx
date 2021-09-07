f075ee8fa91f2a4ed2b9ee1baae0b08a03d3279d
sssajjad007 <sssajjad.76@gmail.com>
2021-09-07 18:33:46 +0430

    create screen for doctorProfile

--- app/doctorProfile/screens/index.tsx
+++ app/doctorProfile/screens/index.tsx
@@ -1,13 +1,41 @@
-//@ts-nocheck
 import React from "react";
-import { View, Image } from "react-native";
-import { MenuItem } from "../components";
-import { MaterialCommunityIcons,FontAwesome5   } from "@expo/vector-icons";
+import { View, Image, Alert } from "react-native";
+import {
+  MaterialCommunityIcons,
+  FontAwesome5,
+  Octicons,
+  Foundation,
+  MaterialIcons,
+  AntDesign,
+  Ionicons,
+} from "@expo/vector-icons";
 import { Shadow } from "react-native-shadow-2";
-import { Alert } from "react-native";
+
+import { MenuItem } from "../components";
 import { styles } from "./style";
+import { IIconProps } from "./type";
 
 export const DoctorProfile = () => {
+  const renderSupportAgent = (props: IIconProps) => {
+    const { color, size } = props;
+    return <MaterialIcons name={"support-agent"} size={size} color={color} />;
+  };
+  const renderInfocirlceo = (props: IIconProps) => {
+    const { color, size } = props;
+    return <AntDesign name={"infocirlceo"} size={size} color={color} />;
+  };
+  const renderQuestiOnCircleo = (props: IIconProps) => {
+    const { color, size } = props;
+    return <AntDesign name={"questioncircleo"} size={size} color={color} />;
+  };
+  const renderExitOutline = (props: IIconProps) => {
+    const { color, size } = props;
+    return <Ionicons name={"exit-outline"} size={size} color={color} />;
+  };
+  const renderClipboardNotes = (props: IIconProps) => {
+    const { color, size } = props;
+    return <Foundation name={"clipboard-notes"} size={size} color={color} />;
+  };
   return (
     <View style={styles.container}>
       <Image
@@ -18,47 +46,28 @@ export const DoctorProfile = () => {
       <Shadow distance={10} viewStyle={styles.bodyShadow}>
         <View style={styles.body}>
           <MenuItem
-            Icon={(color, size) => (
-              <MaterialCommunityIcons
-                name="account-circle"
-                size={size}
-                color={color}
-              />
-            )}
-            title="مشخصات شخصی"
+            Icon={renderSupportAgent}
+            title="پشتیبانی"
             onPress={() => Alert.alert("hi1")}
           />
-           <MenuItem
-            Icon={(color, size) => (
-              <FontAwesome5
-                name="tasks"
-                size={size}
-                color={color}
-              />
-            )}
-            title="تمرین"
+          <MenuItem
+            Icon={renderInfocirlceo}
+            title="درباره ما"
             onPress={() => Alert.alert("hi1")}
           />
-           <MenuItem
-            Icon={(color, size) => (
-              <Octicons 
-                name="checklist"
-                size={size}
-                color={color}
-              />
-            )}
-            title="فرم ها"
+          <MenuItem
+            Icon={renderQuestiOnCircleo}
+            title="سوالات متداول"
             onPress={() => Alert.alert("hi1")}
           />
-           <MenuItem
-            Icon={(color, size) => (
-              <MaterialCommunityIcons
-                name="clipboard-notes"
-                size={size}
-                color={color}
-              />
-            )}
-            title="یادداشت"
+          <MenuItem
+            Icon={renderClipboardNotes}
+            title="شرایط و قوانین "
+            onPress={() => Alert.alert("hi1")}
+          />
+          <MenuItem
+            Icon={renderExitOutline}
+            title="خروج از حساب کاربری"
             onPress={() => Alert.alert("hi1")}
           />
         </View>
---
+++ app/doctorProfile/screens/type.ts
@@ -0,0 +1,8 @@
+
+
+
+export interface IIconProps {
+    color: string;
+    size: number;
+  }
+  
