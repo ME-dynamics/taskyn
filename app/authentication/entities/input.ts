6847f110a0288b41b243e168179024ed1b8bc849
eyousefifar <eyousefifar@gmail.com>
2021-09-13 17:43:06 +0430

    otp validation , and setters

--- app/authentication/entities/input.ts
+++ app/authentication/entities/input.ts
@@ -1,4 +1,4 @@
-import { makeObservable, observable } from "mobx";
+import { action, makeObservable, observable } from "mobx";
 
 export class Input {
   constructor() {
@@ -6,15 +6,27 @@ export class Input {
       phoneNumber: observable,
       otpNumber: observable,
       phoneValidation: observable,
+      otpValidation: observable,
+      setPhoneNumber: action,
+      setOtpNumber: action,
+      setPhoneValidation: action,
+      setOtpValidation: action,
     });
   }
   phoneNumber: string = "";
-  otpNumber: number = -1;
+  otpNumber: string = "";
   phoneValidation: string[] = [];
+  otpValidation: string[] = [];
   setPhoneNumber(phone: string) {
     this.phoneNumber = phone;
   }
+  setOtpNumber(otp: string) {
+    this.otpNumber = otp;
+  }
   setPhoneValidation(validation: string[]) {
     this.phoneValidation = validation;
+  }
+  setOtpValidation(validation: string[]) {
+    this.otpValidation = validation;
   }
 }
