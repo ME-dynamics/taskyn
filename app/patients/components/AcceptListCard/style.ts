bb74dd5c05deb8b28c8e1fc59528dca60bcbf5ce
sssajjad007 <sssajjad.76@gmail.com>
2021-09-13 17:58:00 +0430

    create acceptList component

---
+++ app/patients/components/AcceptListCard/style.ts
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+import { widthPercentageToDP } from "react-native-responsive-screen";
+import { colors } from "../../../library/theme";
+export const styles = StyleSheet.create({
+  container: {
+    width: widthPercentageToDP("100"),
+    height: 120,
+    flexDirection: "row-reverse",
+    backgroundColor: "white",
+    borderRadius: 8,
+    elevation: 2,
+    overflow: "hidden",
+  },
+  imageContainer: {
+    flex: 3,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  patientImage: {
+    width: 76,
+    height: 86,
+    borderRadius: 22 / 2,
+  },
+  infoContainer: {
+    flex: 9,
+   
+  },
+  titleContainer: {
+    flex: 3,
+    justifyContent: "center",
+  },
+  descriptionContainer: {
+    flex: 4,
+    justifyContent: "flex-start",
+  },
+  buttonContainer:{
+    flexDirection:"row",
+    justifyContent:"flex-end",
+    bottom:15,
+  }
+});
