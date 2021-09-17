7028116ced85c948b0d54c8eef6cffd5fb4e4a37
eyousefifar <eyousefifar@gmail.com>
2021-09-17 20:35:53 +0430

    fetch passwodless verify types

--- app/authentication/types/adapters.ts
+++ app/authentication/types/adapters.ts
@@ -2,3 +2,12 @@ export interface IFetchPasswordlessStartResult {
   otpToken: string | undefined;
   error: string | undefined;
 }
+
+export interface IFetchPasswordlessVerifyResult {
+  jwt: string | undefined;
+  refreshToken: string | undefined;
+  refreshExpires: number | undefined;
+  jwtExpires: number | undefined;
+  role: string | undefined;
+  error: string | undefined;
+}
