2364ac129022e3ceb532b783acbd4ea21d3a5b66
eyousefifar <eyousefifar@gmail.com>
2021-09-17 20:39:16 +0430

    fix styles

--- app/task/components/style.ts
+++ app/task/components/style.ts
@@ -4,21 +4,20 @@ import { widthPercentageToDP } from "react-native-responsive-screen";
 export const styles = StyleSheet.create({
   checkboxContainer: {
     width: widthPercentageToDP("86"),
-    minHeight: 50,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "flex-end",
+    minHeight: 56,
+    flexDirection: "column",   
     backgroundColor: "white",
     borderRadius: 8,
     elevation: 3,
   },
   textContainer: {
     flexShrink: 1,
-    margin: 4,
-    marginRight:10,
+    padding:8,
   },
   buttonContainer:{
     flexDirection:"row",
     alignItems:"center",
+    justifyContent: "space-evenly",
+    paddingHorizontal: 32
   }
 });
