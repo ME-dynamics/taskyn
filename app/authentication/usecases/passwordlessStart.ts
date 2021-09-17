70ea2ba7f93d7d42709030058e58f835f1d63a40
eyousefifar <eyousefifar@gmail.com>
2021-09-17 20:45:59 +0430

    move to input state

--- app/authentication/usecases/passwordlessStart.ts
+++ app/authentication/usecases/passwordlessStart.ts
@@ -1,18 +1,18 @@
-import { fetchPasswordlessStart } from "../adapters"
-import { auth } from "../entities"
-
-
-
+import { fetchPasswordlessStart } from "../adapters";
+import { input, auth } from "../entities";
 
 export async function passwordlessStart() {
-    if(auth.phoneNumber) {
-        const {otpToken , error} = await fetchPasswordlessStart(auth.phoneNumber);
-        if(error) {
-            // set error
-            return;
-        }
-        auth.setOtpToken(otpToken);
-
-        // change state to number mode
+  if (input.phoneNumber) {
+    const { otpToken, error } = await fetchPasswordlessStart(input.phoneNumber);
+    if (error) {
+      // set error
+      return;
     }
+    if (otpToken) {
+      auth.setOtpToken(otpToken);
+      return;
+    }
+
+    // change state to number mode
+  }
 }
