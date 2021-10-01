a71932c4596117a260d898b1c283acc9f3444530
sssajjad007 <sssajjad.76@gmail.com>
2021-10-01 15:00:08 +0330

    fix tap for button

--- app/form/components/answerCard/index.tsx
+++ app/form/components/answerCard/index.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Paragraph } from "../../../library";
+import { Paragraph, Tap } from "../../../library";
 import { styles } from "./style";
-export  function AnswerCard() {
+export function AnswerCard() {
   return (
-    <View style={styles.answerContainer}>
-      <View style={styles.answerCard}>
-        <View style={styles.textContainer}>
-          <Paragraph>{"علاقه ای به همکاری با مردم ندارم"}</Paragraph>
+    <Tap
+      onPress={() => {
+        console.log("fuck");
+      }}
+    >
+      <View style={styles.answerContainer}>
+        <View style={styles.answerCard}>
+          <View style={styles.textContainer}>
+            <Paragraph>{"علاقه ای به همکاری با مردم ندارم"}</Paragraph>
+          </View>
         </View>
       </View>
-    </View>
+    </Tap>
   );
 }
