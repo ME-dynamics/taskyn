d7e36e52ba220093e8acc035046e98add3bac320
sssajjad007 <sssajjad.76@gmail.com>
2021-10-01 13:37:19 +0330

    create questioneri screen

--- app/form/screens/questionnaire/style.ts
+++ app/form/screens/questionnaire/style.ts
@@ -6,31 +6,11 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  QuestionContainer: {
-    flex: 4,
+  scrollViewContainer: {
+    flex: 6,
   },
-  answerContainer: {
-    flex: 4,
-    alignItems: "center",
-    justifyContent: "flex-start",
-  },
-  answerCart: {
-    width: widthPercentageToDP("86"),
-    height: 54,
-    borderWidth: 1,
-    borderRadius: 8,
-  },
-  answerCartActive: {
-    borderColor: colors.primaryPurple,
-  },
-  answerCartDeActive: {
-    borderColor: colors.transparentPurple,
-  },
-  textContainer: {
+  scrollview: {
     flex: 1,
-    alignItems: "flex-end",
-    justifyContent: "center",
-    paddingRight: 10,
   },
 
   buttonContainer: {
