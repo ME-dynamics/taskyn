15920bf53cc63a83e20acef6065bcf04cf209b22
eyousefifar <eyousefifar@gmail.com>
2021-10-17 16:29:07 +0330

    change profile ui

--- app/profile/interfaces/components/MenuItem/index.tsx
+++ app/profile/interfaces/components/MenuItem/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 import { View, Text } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Button, Touchable } from "../../../../library";
+import { MaterialCommunityIcons, FontAwesome5 } from "@expo/vector-icons";
+import { Button, Paragraph, Touchable } from "../../../../library";
 import { styles } from "./style";
 import { IMenuItemProps } from "./type";
 export function MenuItem(props: IMenuItemProps) {
@@ -11,8 +11,13 @@ export function MenuItem(props: IMenuItemProps) {
     <View style={styles.container}>
       <View style={[styles.collapsedContainer, styles.plainHeight]}>
         <Icon size={30} color={"grey"} />
-        <Text style={styles.text}>{title}</Text>
+        <View style={{ flex: 1, alignItems: "flex-end", paddingRight: 12 }}>
+          <Paragraph>{title}</Paragraph>
+        </View>
         <MaterialCommunityIcons name="chevron-left" color={"grey"} size={30} />
+      </View>
+      <View style={styles.line}>
+
       </View>
       <Touchable onPress={onPress} rippleColor={"lightGrey"} />
     </View>
