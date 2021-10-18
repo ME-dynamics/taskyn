cc4cd6cb924de5cce5207eb3b9b39b9f9b921ee2
eyousefifar <eyousefifar@gmail.com>
2021-10-18 18:09:27 +0330

    component types

--- app/note/types/components.ts
+++ app/note/types/components.ts
@@ -1,4 +1,16 @@
 export interface IPickerButtonProps {
   mode: "camera" | "gallery";
   onPress: () => void;
+  small?: boolean;
+}
+
+export interface ILineProps {
+  direction?: "vertical" | "horizontal";
+}
+
+export interface INoteImageProps {
+  uri: string;
+  path: string;
+  onCropPress: (path: string) => void;
+  onRemovePress: (path: string) => void;
 }
