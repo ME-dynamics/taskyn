33c1dea163c7211cb37b427a70b90900a9de1835
eyousefifar <eyousefifar@gmail.com>
2021-10-18 18:08:13 +0330

    add picker to note screen

--- app/note/screens/note/index.tsx
+++ app/note/screens/note/index.tsx
@@ -1,30 +1,18 @@
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
 import { View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { Button, THEME, Input, Subheading, Touchable } from "../../../library";
-import { PickerButton, Line } from "../../components";
+import { Button, Input } from "../../../library";
+import { PickerButton, Line, Picker } from "../../components";
 import { styles } from "./styles";
 export function NoteScreen() {
   return (
     <KeyboardAwareScrollView style={styles.container}>
-      <View style={styles.imageContainer}>
-        <View style={styles.cameraIcon}>
-          <PickerButton
-            mode={"gallery"}
-            onPress={() => {
-              console.log("gallery pick");
-            }}
-          />
-          <Line />
-          <PickerButton
-            mode={"camera"}
-            onPress={() => {
-              console.log("camera pick");
-            }}
-          />
+      {/* <View style={styles.pickerContainer}>
+        <View style={styles.picker}>
+          
         </View>
-      </View>
+      </View> */}
+      <Picker />
       <View style={styles.bodyContainer}>
         <View style={styles.inputContainer}>
           <Input mode={"flat"} title={"موضوع"} />
