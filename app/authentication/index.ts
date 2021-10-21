5e2e6971c2b38204b6614e91240d1fd1a57e3a9a
eyousefifar <eyousefifar@gmail.com>
2021-10-21 16:38:10 +0330

    export jwt, and init token func

--- app/authentication/index.ts
+++ app/authentication/index.ts
@@ -1,8 +1,10 @@
 export { Authentication } from "./interfaces";
 import { auth } from "./entities";
-
+export { initToken } from "./usecases";
 export function getLoggedIn() {
   return auth.loggedIn;
 }
 
-
+export function getJWT() {
+  return auth.token;
+}
