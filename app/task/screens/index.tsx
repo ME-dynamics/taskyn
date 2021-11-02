baa86f2838a9d149102d5aff876e5298e96b9937
eyousefifar <eyousefifar@gmail.com>
2021-11-02 15:21:48 +0330

    fix style

--- app/task/screens/index.tsx
+++ app/task/screens/index.tsx
@@ -1,20 +1,41 @@
-import React, { useState } from "react";
-import { View, Text, ScrollView } from "react-native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { View, Text, ScrollView, Keyboard } from "react-native";
 import { Button, Input, Title, Paragraph } from "../../library";
 import { styles } from "./style";
-import { TaskItem } from "../components";
+import { TaskItem } from "../components/TaskItem";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useNavigation } from "@react-navigation/core";
 
 export const Task = () => {
-  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "فرم نيو",
+      headerRight: () => <Title>{"بخش تمرینات"}</Title>,
+    });
+  }, [navigation]);
+  const [value, setValue] = useState("");
+  const [keyboard, setKeyboard] = useState<boolean>(false);
+  useEffect(() => {
+    Keyboard.addListener("keyboardDidShow", () => {
+      setKeyboard(true);
+    });
+    Keyboard.addListener("keyboardDidHide", () => {
+      setKeyboard(false);
+    });
+    return () => {
+      Keyboard.removeAllListeners("keyboardDidShow");
+      Keyboard.removeAllListeners("keyboardDidHide");
+    };
+  }, []);
   return (
     <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Title>{"بخش تمرینات"}</Title>
-      </View>
+      {/* <View style={styles.titleContainer}></View> */}
       <View style={styles.inputContainer}>
         <Input
           mode={"flat"}
+          value={value}
+          onChangeText={setValue}
           title={"اضافه کردن تمرین : "}
           placeholder={"تمرین جدید را وارد کنید."}
         />
@@ -24,40 +45,29 @@ export const Task = () => {
           style={styles.container}
           contentContainerStyle={styles.containerContentStyle}
         >
-          {/* <ScrollView style={styles.scrollViewContainer} contentContainerStyle={styles.containerContentStyle}> */}
           <TaskItem
             text={
               "دیگه نمردی خیلی جون سختی زیر ماشین سیانور اینارو تست کندیونه محترم سر ساعت 9 خودتو پرت کن پایین از پنجره اگه نمردی دوباره این کشس دیشسدی هشسخدی هخشسدی سش دسشهیدسشهیدشس یدشسیدسشیدشسنم دیشسمنیدشمسس شدسیدسشنیدشسن دیمشسد سسارو انجام بده"
             }
             date={"۱۳۷۶/۶/۶"}
           />
-          <TaskItem
-            text={
-              "دیگه نمردی خیلی جون سختی زیر ماشین سیانور اینارو تست کندیونه محترم سر ساعت 9 خودتو پرت کن پایین از پنجره اگه نمردی دوباره این کشس دیشسدی هشسخدی هخشسدی سش دسشهیدسشهیدشس یدشسیدسشیدشسنم دیشسمنیدشمسس شدسیدسشنیدشسن دیمشسد سسارو انجام بده"
-            }
-            date={"1376/6/6"}
-          />
-          <TaskItem
-            text={
-              "دیگه نمردی خیلی جون سختی زیر ماشین سیانور اینارو تست کندیونه محترم سر ساعت 9 خودتو پرت کن پایین از پنجره اگه نمردی دوباره این کشس دیشسدی هشسخدی هخشسدی سش دسشهیدسشهیدشس یدشسیدسشیدشسنم دیشسمنیدشمسس شدسیدسشنیدشسن دیمشسد سسارو انجام بده"
-            }
-            date={"1376/6/6"}
-          />
-          <TaskItem
-            text={
-              "دیگه نمردی خیلی جون سختی زیر ماشین سیانور اینارو تست کندیونه محترم سر ساعت 9 خودتو پرت کن پایین از پنجره اگه نمردی دوباره این کشس دیشسدی هشسخدی هخشسدی سش دسشهیدسشهیدشس یدشسیدسشیدشسنم دیشسمنیدشمسس شدسیدسشنیدشسن دیمشسد سسارو انجام بده"
-            }
-            date={"1376/6/6"}
-          />
         </KeyboardAwareScrollView>
+      </View>
 
-        {/* </ScrollView> */}
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button mode={"contained"} size={"big"} rippleColor={"lightGrey"}>
-          {"اضافه کردن تمرین"}
-        </Button>
-      </View>
+      {keyboard ? null : (
+        <View style={styles.buttonContainer}>
+          <Button
+            mode={"contained"}
+            size={"big"}
+            rippleColor={"lightGrey"}
+            onPress={() => {
+              <TaskItem text={value} date={"1376/6/6"} />;
+            }}
+          >
+            {"اضافه کردن تمرین"}
+          </Button>
+        </View>
+      )}
     </View>
   );
 };
--- app/task/screens/style.ts
+++ app/task/screens/style.ts
@@ -1,39 +1,42 @@
 import { StyleSheet } from "react-native";
-import { heightPercentageToDP, widthPercentageToDP } from "react-native-responsive-screen";
+import {
+  heightPercentageToDP,
+  widthPercentageToDP,
+} from "react-native-responsive-screen";
 import { THEME } from "../../library";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:THEME.COLORS.BACKGROUND,
+    backgroundColor: THEME.COLORS.BACKGROUND,
   },
   titleContainer: {
-    width:widthPercentageToDP("100"),
-    height:heightPercentageToDP("10"),
+    width: widthPercentageToDP("100"),
+    height: heightPercentageToDP("10"),
     alignItems: "center",
     justifyContent: "center",
   },
   inputContainer: {
-    width:widthPercentageToDP("100"),
-    height:heightPercentageToDP("10"),
+    width: widthPercentageToDP("100"),
+    height: heightPercentageToDP("10"),
     alignItems: "center",
     justifyContent: "center",
   },
   taskContainer: {
-    width:widthPercentageToDP("100"),
-    height:heightPercentageToDP("60"),
+    // width: widthPercentageToDP("100"),
+    // height: heightPercentageToDP("60"),
+    flex: 1,
     paddingTop: 20,
     //width: widthPercentageToDP("86"),
-   
   },
-  scrollViewContainer:{
-    flex:1,
+  scrollViewContainer: {
+    flex: 1,
   },
-  containerContentStyle:{alignItems:"center"},
+  containerContentStyle: { alignItems: "center" },
   buttonContainer: {
-    width:widthPercentageToDP("100"),
-    height:heightPercentageToDP("10"),
-        alignItems: "center",
+    width: widthPercentageToDP("100"),
+    height: heightPercentageToDP("10"),
+    alignItems: "center",
     justifyContent: "center",
   },
 });
