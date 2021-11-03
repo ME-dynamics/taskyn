b77e4e9cd158fb249ae24f1dfe55d9c2ee748835
eyousefifar <eyousefifar@gmail.com>
2021-11-03 12:31:06 +0330

    fetch tasks implementation

---
+++ app/task/adapters/network/fetchTasks.ts
@@ -0,0 +1,49 @@
+import { toJalaaliDate } from "../utils";
+import { request } from "../../../library";
+import { IFetchTasksPayload, ITask } from "../../types";
+
+export async function fetchTasks(): Promise<IFetchTasksPayload> {
+  const { error, httpStatus, payload, success } = await request({
+    body: undefined,
+    endpoint: "/tasks/",
+    method: "GET",
+    token: "",
+  });
+  if (!success) {
+    return {
+      error: String(error),
+      tasks: [],
+    };
+  }
+  if (Array.isArray(payload)) {
+    const result: ITask[] = [];
+    for (let index = 0; index < payload.length; index++) {
+      const task = payload[index];
+
+      const createdAt =
+        Date.parse(task?.createdAt) !== NaN
+          ? toJalaaliDate(new Date(task?.createdAt))
+          : "";
+      const modifiedAt =
+        Date.parse(task?.modifiedAt) !== NaN
+          ? toJalaaliDate(new Date(task?.modifiedAt))
+          : "";
+      result.push({
+        userId: String(task?.userId),
+        id: String(task?.id),
+        content: String(task?.content),
+        done: Boolean(task?.done),
+        createdAt,
+        modifiedAt,
+      });
+    }
+    return {
+      error: "",
+      tasks: result,
+    };
+  }
+  return {
+    error: "must be array",
+    tasks: [],
+  };
+}
