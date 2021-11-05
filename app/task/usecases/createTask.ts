ab588773b1f7580ed088015fc470eb9abbec39c1
eyousefifar <eyousefifar@gmail.com>
2021-11-05 19:40:14 +0330

    validate task to avoid empty tasks

--- app/task/usecases/createTask.ts
+++ app/task/usecases/createTask.ts
@@ -2,11 +2,15 @@ import { fetchCreateTask } from "../adapters";
 import { taskState } from "../entities";
 
 export async function createTask() {
+  if (taskState.newTask.length < 3) {
+    taskState.setTaskValidation(["تسک خود رو به طور کامل تعریف کنید."]);
+    return;
+  }
   const { error, task } = await fetchCreateTask(taskState.newTask);
   if (error) {
     taskState.setNewTask("");
     return;
   }
-  taskState.addList(task);
+  taskState.addTask(task);
   taskState.setNewTask("");
 }
