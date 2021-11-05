20a357e54d9081aafa12ae2f4be8871c7707374f
eyousefifar <eyousefifar@gmail.com>
2021-11-05 19:38:47 +0330

    better naming, better computed task algo data modification methods

--- app/task/entities/Task.ts
+++ app/task/entities/Task.ts
@@ -3,46 +3,76 @@ import { ITask } from "../types";
 export class TaskState {
   constructor() {
     makeObservable(this, {
-      list: observable,
+      tasks: observable,
       newTask: observable,
+      taskValidation: observable,
+      setTaskValidation: action,
       setNewTask: action,
-      setList: action,
-      addList: action,
-      doneList: computed,
-      undoneList: computed,
+      setTasks: action,
+      addTask: action,
+      updateTask: action,
+      removeTask: action,
+      setTaskDone: action,
+      taskList: computed,
     });
   }
   newTask: string = "";
-  list: ITask[] = [];
+  taskValidation: string[] = [];
+  tasks: ITask[] = [];
   setNewTask(content: string) {
     this.newTask = content;
   }
-  setList(list: ITask[]) {
-    this.list = list;
+  setTaskValidation(validations: string[]) {
+    this.taskValidation = validations;
   }
-  addList(task: ITask) {
-    this.list.unshift(task);
+  setTasks(list: ITask[]) {
+    this.tasks = list;
   }
-  get doneList() {
-    const done: ITask[] = [];
-    const len = this.list.length;
-    for (let index = 0; index < len; index++) {
-      const task = this.list[index];
-      if (task.done) {
-        done.push(task);
+  addTask(task: ITask) {
+    this.tasks.unshift(task);
+  }
+  updateTask(task: ITask) {
+    for (let index = 0; index < this.tasks.length; index++) {
+      const id = this.tasks[index].id;
+      if (id === task.id) {
+        this.tasks[index] = task;
+        break;
       }
     }
-    return done;
   }
-  get undoneList() {
+  removeTask(taskId: string) {
+    const result: ITask[] = [];
+    for (let index = 0; index < this.tasks.length; index++) {
+      const task = this.tasks[index];
+      if (task.id === taskId) {
+        continue;
+      }
+      result.push(task);
+    }
+    this.tasks = result;
+  }
+  setTaskDone(taskId: string) {
+    for (let index = 0; index < this.tasks.length; index++) {
+      const id = this.tasks[index].id;
+      if (id === taskId) {
+        this.tasks[index].done = true;
+        break;
+      }
+    }
+  }
+  get taskList() {
+    const done: ITask[] = [];
     const undone: ITask[] = [];
-    const len = this.list.length;
+    const len = this.tasks.length;
     for (let index = 0; index < len; index++) {
-      const task = this.list[index];
-      if (!task.done) {
+      const task = this.tasks[index];
+      if (task.done) {
+        done.push(task);
+      } else {
         undone.push(task);
       }
     }
-    return undone;
+    // TODO: this line can be optimized further
+    return done.concat(undone);
   }
 }
