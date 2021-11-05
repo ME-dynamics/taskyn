90bf3ca9685a3e8d195f0d974d2b489ca84be934
eyousefifar <eyousefifar@gmail.com>
2021-11-05 19:40:29 +0330

    remove error on validated task

--- app/task/usecases/onNewTaskChange.ts
+++ app/task/usecases/onNewTaskChange.ts
@@ -1,5 +1,10 @@
 import { taskState } from "../entities";
 
 export function onNewTaskChange(text: string) {
+  if (taskState.taskValidation.length > 0) {
+    if (text.length > 3) {
+      taskState.setTaskValidation([]);
+    }
+  }
   taskState.setNewTask(text);
 }
