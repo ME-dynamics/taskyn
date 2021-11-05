2d2adc39836a72a5deabb2aed3838fe72f6ae98e
eyousefifar <eyousefifar@gmail.com>
2021-11-05 19:38:55 +0330

    new types

--- app/task/types/adapters.ts
+++ app/task/types/adapters.ts
@@ -8,3 +8,9 @@ export interface IFetchCreateTaskPayload {
   error: string;
   task: ITask;
 }
+
+export interface IFetchUpdateTask {
+  taskId: string;
+  content: string;
+  userId: string | undefined;
+}
