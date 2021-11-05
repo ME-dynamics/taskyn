00cd312c80fba1c38a52e857138789c624fc5ba6
eyousefifar <eyousefifar@gmail.com>
2021-11-05 19:41:29 +0330

    export new usecases

--- app/task/usecases/index.ts
+++ app/task/usecases/index.ts
@@ -1,3 +1,6 @@
 export { retrieveTasks } from "./retrieveTasks";
 export { createTask } from "./createTask";
 export { onNewTaskChange } from "./onNewTaskChange";
+export { taskDone } from "./taskDone";
+export { updateTask } from "./updateTask";
+export { removeTask } from "./removeTask"
