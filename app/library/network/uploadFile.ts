2c21c2ce564b12fcd4b70461048a1041389988fe
eyousefifar <eyousefifar@gmail.com>
2021-11-08 11:59:49 +0330

    implement upload file, add token cache

---
+++ app/library/network/uploadFile.ts
@@ -0,0 +1,68 @@
+import { uploadAsync, FileSystemUploadType } from "expo-file-system";
+import { secureStorage, storage } from "../storage";
+import { serverUrl, jwtPayload } from "./constant";
+import { IResponse, IUploadFile, tFileType } from "./types";
+
+function endpointGen(type: tFileType) {
+  switch (type) {
+    case "image":
+      return `${serverUrl}/images/upload`;
+    default:
+      return "no where";
+  }
+}
+
+export function buildUploadFile() {
+  let tokenCache = "";
+  let tokenCacheValidTime = 0;
+  async function setToken() {
+    const [jwtToken, tokenExpiresAt] = await Promise.all([
+      secureStorage.retrieve("token"),
+      storage.retrieve("refresh_expires_at"),
+    ]);
+    tokenCache = jwtToken ? jwtToken : "";
+    tokenCacheValidTime = tokenExpiresAt ? parseInt(tokenExpiresAt, 10) : 0;
+  }
+  async function getJwtToken() {
+    if (tokenCache && tokenCacheValidTime) {
+      if (tokenCacheValidTime > Date.now()) {
+        await setToken();
+      }
+      return tokenCache;
+    }
+    await setToken();
+    return tokenCache;
+  }
+  return async function uploadFile(info: IUploadFile): Promise<IResponse> {
+    const { path, type } = info;
+    const endPoint = endpointGen(type);
+    const { body, headers, mimeType, status } = await uploadAsync(
+      endPoint,
+      path,
+      {
+        fieldName: "imageFile",
+        httpMethod: "POST",
+        uploadType: FileSystemUploadType.MULTIPART,
+        headers: {
+          Authorization: (await getJwtToken()) || "no-token",
+          "Content-type": "multipart/form-data",
+          Accept: "application/json",
+          "Accept-Encoding": "gzip",
+          "x-jwt-payload": jwtPayload,
+        },
+        parameters: {
+          access: "private",
+          transform: "note",
+        },
+      }
+    );
+    const success = status >= 200 && status <= 299;
+    const data = JSON.parse(body);
+    return {
+      success,
+      error: success ? "" : data,
+      httpStatus: status,
+      payload: success ? data : "",
+    };
+  };
+}
