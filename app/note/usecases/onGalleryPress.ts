167ac2cac2f6152425051f6cae561dd42639e670
eyousefifar <eyousefifar@gmail.com>
2021-11-08 12:02:33 +0330

    use upload file, make it faster

--- app/note/usecases/onGalleryPress.ts
+++ app/note/usecases/onGalleryPress.ts
@@ -1,17 +1,19 @@
-import { uploadNoteImage } from "../adapters";
 import { note } from "../entities";
-import { openGallery } from "../../library";
+import { openGallery, uploadFile } from "../../library";
 
 export async function onGalleryPress() {
   const images = await openGallery("note");
+  const uploadPromises = [];
   if (Array.isArray(images)) {
     for (let index = 0; index < images.length; index++) {
       const image = images[index];
       note.addImage(image.path);
-      await uploadNoteImage(image.path);
+      uploadPromises.push(uploadFile({ path: image.path, type: "image" }));
     }
+    const result = await Promise.all(uploadPromises);
+    // TODO: add results to note images;
     return;
   }
   note.addImage(images.path);
-  await uploadNoteImage(images.path);
+  await uploadFile({ path: images.path, type: "image" });
 }
