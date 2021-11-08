6a24c99f49258d7bb1a29608eef03ab62c47c067
eyousefifar <eyousefifar@gmail.com>
2021-11-08 11:59:24 +0330

    add token cache to request

---
+++ app/library/network/request.ts
@@ -0,0 +1,59 @@
+import { secureStorage, storage } from "../storage";
+import { serverUrl, jwtPayload } from "./constant";
+import { IRequest, IResponse } from "./types";
+
+export function buildRequest() {
+  let tokenCache = "";
+  let tokenCacheValidTime = 0;
+  async function setToken() {
+    const [jwtToken, tokenExpiresAt] = await Promise.all([
+      secureStorage.retrieve("token"),
+      storage.retrieve("refresh_expires_at"),
+    ]);
+    tokenCache = jwtToken ? jwtToken : "";
+    tokenCacheValidTime = tokenExpiresAt ? parseInt(tokenExpiresAt, 10) : 0;
+  }
+  async function getJwtToken() {
+    if (tokenCache && tokenCacheValidTime) {
+      if (tokenCacheValidTime > Date.now()) {
+        await setToken();
+      }
+      return tokenCache;
+    }
+    await setToken();
+    return tokenCache;
+  }
+  return async function request(info: IRequest): Promise<IResponse> {
+    try {
+      const { method, endpoint, body } = info;
+      const response: Response = await fetch(`${serverUrl}${endpoint}`, {
+        method,
+        headers: {
+          Authorization: (await getJwtToken()) || "no-token",
+          "Content-type": "application/json",
+          Accept: "application/json",
+          "Accept-Encoding": "gzip",
+          "x-jwt-payload": jwtPayload,
+        },
+        body: body ? JSON.stringify(body) : undefined,
+      });
+      const data = await response.json();
+      const { ok, status } = response;
+      return {
+        success: ok,
+        httpStatus: status,
+        payload: ok ? data.payload : undefined,
+        error: !ok ? data?.error : undefined,
+      };
+    } catch (err) {
+      // show error
+      // add sentry
+      return {
+        success: false,
+        httpStatus: 0,
+        payload: undefined,
+        error: undefined,
+      };
+    }
+  };
+}
