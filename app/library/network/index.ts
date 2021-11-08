80583d3520c426a3f1d411d1b027feb305be2471
eyousefifar <eyousefifar@gmail.com>
2021-11-08 12:00:21 +0330

    build and export network functions

--- app/library/network/index.ts
+++ app/library/network/index.ts
@@ -1,38 +1,5 @@
-import { IRequest, IResponse } from "./types";
+import { buildRequest } from "./request";
+import { buildUploadFile } from "./uploadFile";
 
-const serverUrl = "https://foolish-dragon-50.loca.lt/api/v1";
-
-export async function request(info: IRequest): Promise<IResponse> {
-  try {
-    const { token, method, endpoint, body } = info;
-    const response: Response = await fetch(`${serverUrl}${endpoint}`, {
-      method,
-      headers: {
-        Authorization: token || "no-token",
-        "Content-type": "application/json",
-        Accept: "application/json",
-        "Accept-Encoding": "gzip",
-        "x-jwt-payload":
-          "eyJjdXN0b21lciI6dHJ1ZSwiaXNzIjoiaHR0cHM6Ly90YXNreW4uaXIiLCJzdWIiOiI5MzBhMGZhOS1jODUwLTQ0MzQtYTZiMS02Mjk1NmE0Y2VjODUiLCJhdWQiOiJ0YXNreW4iLCJqdGkiOiJYZVlyazgiLCJpYXQiOjE2MzQ2NjcxNjksIm5iZiI6MTYzNDY2NzE2OSwiZXhwIjoxNjQwNjY3MTY5fQ",
-      },
-      body: body ? JSON.stringify(body) : undefined,
-    });
-    const data = await response.json();
-    const { ok, status } = response;
-    return {
-      success: ok,
-      httpStatus: status,
-      payload: ok ? data.payload : undefined,
-      error: !ok ? data?.error : undefined,
-    };
-  } catch (err) {
-    // show error
-    // add sentry
-    return {
-      success: false,
-      httpStatus: 0,
-      payload: undefined,
-      error: undefined,
-    };
-  }
-}
+export const request = buildRequest();
+export const uploadFile = buildUploadFile();
