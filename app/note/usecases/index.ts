b577ab50a368f266193014eda50dfa1c19a23088
eyousefifar <eyousefifar@gmail.com>
2021-11-08 18:50:29 +0330

    export new usecases

--- app/note/usecases/index.ts
+++ app/note/usecases/index.ts
@@ -2,3 +2,6 @@ export { onCameraPress } from "./onCameraPress";
 export { onCropPress } from "./onCropPress";
 export { onRemovePress } from "./onRemovePress";
 export { onGalleryPress } from "./onGalleryPress";
+export { onTitleChange } from "./onTitleChange";
+export { onContentChange } from "./onContentChange";
+export { createNote } from "./createNote";
