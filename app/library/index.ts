a1f22e042fb5ab34a5716a20f2af4a9bc64c9b78
eyousefifar <eyousefifar@gmail.com>
2021-11-08 12:00:45 +0330

    export upload file

--- app/library/index.ts
+++ app/library/index.ts
@@ -4,7 +4,7 @@ export { THEME } from "./theme";
 export { Touchable } from "./Touchable";
 export { Caption, Headline, Paragraph, Subheading, Title } from "./Typography";
 export { SearchBar } from "./SearchBar";
-export { request } from "./network";
+export { request, uploadFile } from "./network";
 export { secureStorage, storage } from "./storage";
 export { Tap } from "./Tap";
 export { openCamera, openCropper, openGallery } from "./cropPicker";
