24d3c9a60043176caebfc8798cc888f8ed5c3efb
eyousefifar <eyousefifar@gmail.com>
2021-11-27 18:53:10 +0330

    userInfo types

---
+++ app/userInfo/types/entities.ts
@@ -0,0 +1,61 @@
+export type tEducation =
+  | "uneducated"
+  | "underAged"
+  | "preSchool"
+  | "school"
+  | "preHighSchool"
+  | "highSchool"
+  | "dropoutPreSchool"
+  | "dropoutSchool"
+  | "dropoutSchool"
+  | "student"
+  | "graduate"
+  | "diploma"
+  | "clergyman";
+export type tGender = "male" | "female";
+
+export type tMaritalStatus = "engaged" | "single";
+export type tMaritalState =
+  | "namzad"
+  | "aghed"
+  | "aghedDayem"
+  | "movaghat"
+  | "wives"
+  | "motarekeh"
+  | "motalagheh"
+  | "widow";
+
+export type tReligion =
+  | "islamShia"
+  | "islamSunny"
+  | "zoroastrianism" // zartoshti
+  | "christianity"
+  | "other";
+
+export interface IUserInfo {
+  firstName: string | undefined;
+  lastName: string | undefined;
+  description: string | undefined;
+  gender: tGender | undefined;
+  birthday: Date | undefined;
+  address: string | undefined;
+  telephone: string | undefined;
+  problemDescription: string | undefined;
+  maritalStatus: tMaritalStatus | undefined;
+  maritalState: tMaritalState | undefined;
+  education: tEducation | undefined;
+  academicField: string | undefined;
+  religion: tReligion | undefined;
+  job: string | undefined;
+  bodyDiseases: string | undefined;
+  mindDiseases: string | undefined;
+  drugUse: string | undefined;
+  addiction: string | undefined;
+  isFatherAlive: boolean | undefined;
+  isMotherAlive: boolean | undefined;
+  fatherDeathReason: string | undefined;
+  motherDeathReason: string | undefined;
+  cousinMarriage: boolean | undefined;
+  siblingsPosition: number | undefined;
+  siblings: string | undefined;
+}
