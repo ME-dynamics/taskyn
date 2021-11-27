633e3ca3af4653993bf9871a11f866534dfddb48
eyousefifar <eyousefifar@gmail.com>
2021-11-27 18:52:35 +0330

    forward ref for borderless button

--- app/library/IconButton/index.tsx
+++ app/library/IconButton/index.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { forwardRef, Ref } from "react";
 import { BorderlessButton } from "react-native-gesture-handler";
 import { observer } from "mobx-react-lite";
 import { iconStyleGen } from "./styles";
 
 import { IIconButtonProps } from "./types";
 
-function IconButtonComponent(props: IIconButtonProps) {
+function IconButtonComponent(
+  props: IIconButtonProps,
+  ref: Ref<BorderlessButton> | undefined
+) {
   const { Icon, onPress, color, size } = props;
   const { iconColor, iconSize, rippleColor } = iconStyleGen({ color, size });
   return (
-    <BorderlessButton onPress={onPress} rippleColor={rippleColor}>
+    <BorderlessButton ref={ref} onPress={onPress} rippleColor={rippleColor}>
       <Icon color={iconColor} size={iconSize} />
     </BorderlessButton>
   );
 }
 
-export const IconButton = observer(IconButtonComponent);
+export const IconButton = observer(IconButtonComponent, {
+  forwardRef: true,
+});
