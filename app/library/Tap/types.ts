aff0c7051d5069862edd74629368cccd643858cf
eyousefifar <eyousefifar@gmail.com>
2021-11-27 18:52:57 +0330

    waitFor ref

--- app/library/Tap/types.ts
+++ app/library/Tap/types.ts
@@ -1,6 +1,7 @@
-import { ReactChild, ReactChildren } from "react";
+import { ReactChild, ReactChildren, Ref } from "react";
 
 export interface ITapProps {
   children: ReactChild | ReactChildren;
   onPress?: () => void;
+  waitFor?: Ref<unknown> | Ref<unknown>[] | undefined
 }
