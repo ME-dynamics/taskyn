c31909b25a75900f52825b4114c01f67835cbc93
eyousefifar <eyousefifar@gmail.com>
2021-11-30 14:10:50 +0330

    support for string child

--- app/library/SelectButton/index.tsx
+++ app/library/SelectButton/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { View } from "react-native";
+import { observer } from "mobx-react-lite";
 import { Checkbox } from "../CheckBox";
 import { Radio } from "../Radio";
+import { Paragraph } from "../Typography";
 import { Tap } from "../Tap";
-import { observer } from "mobx-react-lite";
 import { styleGen } from "./styles";
 import { ISelectButtonProps } from "./types";
 
@@ -20,12 +21,18 @@ function SelectButtonComponent(props: ISelectButtonProps) {
       onPress(id || "");
     }
   }
+  function renderChild() {
+    if (typeof children === "string") {
+      return <Paragraph>{children}</Paragraph>;
+    }
+    return children;
+  }
   const styles = styleGen(size);
   return (
     <Tap onPress={onButtonPress}>
       <View style={styles.container}>
         <View style={styles.selectorContainer}>{renderSelector()}</View>
-        <View style={styles.labelContainer}>{children}</View>
+        <View style={styles.labelContainer}>{renderChild()}</View>
       </View>
     </Tap>
   );
