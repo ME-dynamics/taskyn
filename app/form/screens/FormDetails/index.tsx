fb60a6edcae223fb991a017483a8e1994522f9c0
eyousefifar <eyousefifar@gmail.com>
2021-12-08 20:06:03 +0330

    refactor formDetails ( new ui )

--- app/form/screens/FormDetails/index.tsx
+++ app/form/screens/FormDetails/index.tsx
@@ -3,15 +3,20 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import React, { useLayoutEffect } from "react";
 import { View, Text, Image, ScrollView } from "react-native";
 import { Button, Paragraph, Subheading, THEME, Title } from "../../../library";
+import { FormNumberIcon, TimerIcon } from "../../../library/Icon";
 import { styles } from "./style";
-
+const FormCount = 93;
 export function FormDetails() {
   const navigation = useNavigation<NativeStackNavigationProp<any>>();
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "تست MBTI",
       headerTitleAlign: "center",
-      headerTitle: (props) => <Paragraph style={{fontSize: 18, top: 4, color: "white"}}  >{props.children}</Paragraph>,
+      headerTitle: (props) => (
+        <Paragraph style={{ fontSize: 18, top: 4, color: "white" }}>
+          {props.children}
+        </Paragraph>
+      ),
       headerTintColor: "white",
       headerStyle: {
         backgroundColor: THEME.COLORS.PRIMARY.NORMAL,
@@ -25,36 +30,56 @@ export function FormDetails() {
           <View style={styles.imageContainer}>
             <Image
               style={styles.image}
-              source={require("../../assets/neoForm.jpg")}
+              source={require("../../assets/Neo.png")}
               resizeMode={"stretch"}
             />
           </View>
           <View style={styles.detailsContainer}>
-            <Title>
-              {"پرسشنامه چند جنبه ای شخصیتی مینه چند عاملی فلان سوتا"}
-            </Title>
-            <Subheading style={{ textAlign: "right" }}>
-              {"Revised NEO Personality Inventory"}
+            <Title>{"تست بریگز و مایرز"}</Title>
+            <Subheading style={{ textAlign: "right", top: 10 }}>
+              {"Myers–Briggs Type Indicator"}
             </Subheading>
           </View>
           <View style={styles.descriptionContainer}>
-            <Paragraph>
-              {`در حوضه منابع انسانی سازمان ها: این آزمون در سال های اخیر به عنوان یکی از پرکاربردترین آزمون های استخدام  مورد توجه  شرکت ها و ارگان های بزرگ و کوچک قرار گرفته است. همچنین این تست می تواند عملکرد شغلی را پیش بینی کند به این معنی که آیا او می تواند خروجی مورد نظر را برای شرکت به دست آورد یا خیر؟ و ویژگی های روان شناسی افراد از افسردگی، توانایی های مدیریتی تا هدر حوضه منابع انسانی سازمان ها: این آزمون در سال های اخیر به عنوان یکی از پرکاربردترین آزمون های استخدام  مورد توجه  شرکت ها و ارگان های بزرگ و کوچک قرار گرفته است. همچنین این تست می تواند عملکرد شغلی را پیش بینی کند به این معنی که آیا او می تواند خروجی مورد نظر را برای شرکت به دست آورد یا خیر؟ و ویژگی های روان شناسی افراد از افسردگی، توانایی های مدیریتی تا هوش هیجانی را انجام دهد.در حوضه منابع انسانی سازمان ها: این آزمون در سال های اخیر به عنوان یکی از پرکاربردترین آزمون های استخدام  مورد توجه  شرکت ها و ارگان های بزرگ و کوچک قرار گرفته است. همچنین این تست می تواند عملکرد شغلی را پیش بینی کند به این معنی که آیا او می تواند خروجی مورد نظر را برای شرکت به دست آورد یا خیر؟ و ویژگی های روان شناسی افراد از افسردگی، توانایی های مدیریتی تا هوش هیجانی را انجام دهد.وش هیجانی را انجام دهد.`}
+            <Paragraph style={{ color: "#727272" }}>
+              {`در نظریه یونگ، هشت الگوی رفتاری انسان معرفی شد. بریگز و مایرز ایده‌های یونگ را در مورد این الگوهای ذهنی توسعه دادند و در نهایت نقش آنها را در مفهوم و توصیف انواع شخصیت‌ها در تست MBTI گنجاندند. تست بریگز و مایرز نوعی ارزیابی است که در آن شخصیت فرد را با توجه به چهار معیار اصلی مورد بررسی قرار می‌گیرد که در آن هر حرف مربوط به یک اولویت یا گرایش شخصیتی خاص است. این معیارها برون گرا یا درون گرا بودن، شهودی یا حسی بوده، ادراکی یا قضاوتی بودن و در نهایت احساسی یا منطقی بودن هستند.`}{" "}
             </Paragraph>
           </View>
         </ScrollView>
       </View>
       <View style={styles.buttonContainer}>
-        <Button
-          mode={"contained"}
-          size={"big"}
-          rippleColor={"lightGrey"}
-          onPress={() => {
-            navigation.push("Questionnaire");
+        <View style={styles.formDetails}>
+          <View style={styles.formDetailsBody}>
+            <Paragraph style={{ color: "#727272", right: 10 }}>
+              {"تعداد تست ها: "} <Paragraph>{FormCount}</Paragraph>
+            </Paragraph>
+            <TimerIcon size={24} />
+          </View>
+          <View style={styles.formDetailsBody}>
+            <Paragraph style={{ color: "#727272", right: 8 }}>
+              {"زمان لازم: "} <Paragraph>{"۱۰ دقیقه"}</Paragraph>
+            </Paragraph>
+            <FormNumberIcon size={24} />
+          </View>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "flex-start",
           }}
         >
-          {"ورود به فرم"}
-        </Button>
+          <Button
+            mode={"contained"}
+            size={"big"}
+            rippleColor={"lightGrey"}
+            onPress={() => {
+              navigation.push("Questionnaire");
+            }}
+          >
+            {"شروع تست"}
+          </Button>
+        </View>
       </View>
     </View>
   );
