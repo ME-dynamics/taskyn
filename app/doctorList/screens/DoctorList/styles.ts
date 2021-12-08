18a1536fdee89c93fc632222599721f80f380f09
eyousefifar <eyousefifar@gmail.com>
2021-12-08 17:30:33 +0330

    refactor DoctorList ( new ui )

--- app/doctorList/screens/DoctorList/styles.ts
+++ app/doctorList/screens/DoctorList/styles.ts
@@ -8,8 +8,19 @@ import { THEME } from "../../../library";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: THEME.COLORS.BACKGROUND,
+    backgroundColor: "#f5f5f5",
+    alignItems: "center",
   },
+  scrolViewContainer: {
+    flex: 1,
+  },
+  titleContainer: {
+    width: widthPercentageToDP("100"),
+    height: heightPercentageToDP("10"),
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
   searchBarContainer: {
     width: widthPercentageToDP("100"),
     height: heightPercentageToDP("10"),
@@ -17,13 +28,13 @@ export const styles = StyleSheet.create({
     justifyContent: "space-evenly",
     flexDirection: "row-reverse",
   },
-  doctorCardContainer: {
-    width: widthPercentageToDP("100"),
-    height: heightPercentageToDP("90"),
-    alignItems: "center",
+  doctorsCardContainer: {
+    width: widthPercentageToDP("90"),
+    minHeight: heightPercentageToDP("30"),
+    alignItems: "flex-end",
   },
   iconContainer: {
-    width: widthPercentageToDP("10"),
+    width: widthPercentageToDP("18"),
     height: "100%",
     alignItems: "center",
     justifyContent: "center",
@@ -32,7 +43,7 @@ export const styles = StyleSheet.create({
     height: 20,
     width: 20,
     borderRadius: 20 / 2,
-    backgroundColor: THEME.COLORS.RED,
+    backgroundColor: THEME.COLORS.RED.NORMAL,
   },
   numberStyle: {
     textAlign: "center",
@@ -43,10 +54,25 @@ export const styles = StyleSheet.create({
   redCircleContainer: {
     position: "absolute",
     top: 10,
-    right: 2,
+    right: 40,
+    zIndex: 1000,
   },
   containerContentStyle: {
-     alignItems: "center",
- 
-},
+    alignItems: "center",
+  },
+  icon: {
+    width: 40,
+    height: 40,
+    borderRadius: 32 / 2,
+    backgroundColor: "#EEE5FF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  infoContainer: {
+    flex: 10,
+  },
+  title: {
+    flex: 1,
+    justifyContent: "center",
+  },
 });
