73f1263d4789aede6bb0ae46f31d6bc9fa8eb01e
eyousefifar <eyousefifar@gmail.com>
2021-12-14 22:44:08 +0330

    add comments to net info state

--- app/library/netInfo/netInfoState.ts
+++ app/library/netInfo/netInfoState.ts
@@ -1,4 +1,4 @@
-import { makeObservable, observable, action, computed } from "mobx";
+import { makeObservable, observable, action } from "mobx";
 
 class NetInfo {
   constructor() {
@@ -11,9 +11,26 @@ class NetInfo {
       setVpn: action,
     });
   }
+  /**
+   * if there is an active network connection 
+   * isConnected is set to true
+   * this does not necessarily means internet is reachable
+   */
   isConnected: boolean = true;
+  /**
+   * if connection is available and internet is reachable
+   */
   internetAvailable: boolean = true;
+  /**
+   * if user is using an active vpn connection
+   * this can be used to route user to a better server in client side
+   * although this should be implemented in server side
+   */
   vpn: boolean = false;
+  /**
+   * if connection is active and internet is reachable
+   * use this property for most of your assumption about network access
+   */
   get hasAccess(): boolean {
     return this.isConnected && this.internetAvailable;
   }
