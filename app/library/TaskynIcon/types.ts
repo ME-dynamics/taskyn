4b3f2437a9c01a76edec390428017a8091b816d9
eyousefifar <eyousefifar@gmail.com>
2021-12-14 18:52:12 +0330

    taskyn Icons with types

---
+++ app/library/TaskynIcon/types.ts
@@ -0,0 +1,46 @@
+export type tTaskynIconNames =
+  | "close"
+  | "close-circle"
+  | "users"
+  | "trash"
+  | "timer"
+  | "tests"
+  | "telegram"
+  | "task"
+  | "search"
+  | "repeat"
+  | "profile"
+  | "practice"
+  | "note"
+  | "pencil"
+  | "paperclip"
+  | "notes"
+  | "neo"
+  | "menu"
+  | "mbti"
+  | "logout"
+  | "linkedin"
+  | "law"
+  | "instagram"
+  | "info"
+  | "image"
+  | "home"
+  | "folder"
+  | "help-circle"
+  | "headphones"
+  | "forward"
+  | "form"
+  | "filter"
+  | "file-history"
+  | "file"
+  | "done"
+  | "document"
+  | "disconnected"
+  | "camera"
+  | "back";
+
+export interface ITaskynIconProps {
+  name: tTaskynIconNames;
+  size: number;
+  color: string;
+}
