d6886c9e630f11e4dd1f80126614982ae02eb6a3
eyousefifar <eyousefifar@gmail.com>
2021-12-15 18:14:51 +0330

    add svg icon support

--- app/library/TaskynIcon/index.tsx
+++ app/library/TaskynIcon/index.tsx
@@ -1,14 +1,48 @@
 import React from "react";
 import { createIconSetFromIcoMoon } from "@expo/vector-icons";
-import { ITaskynIconProps } from "./types";
 import { observer } from "mobx-react-lite";
+
+import { File } from "./File";
+import { FileHistory } from "./FileHistory";
+import { Form } from "./Form";
+import { Google } from "./Google";
+import { Instagram } from "./Instagram";
+import { Note } from "./Note";
+import { Practice } from "./Practice";
+
+import { ITaskynIconProps } from "./types";
 const Icon = createIconSetFromIcoMoon(
   require("./selection.json"),
   "TaskynIcon",
   "iconmoon.ttf"
 );
 function TaskynIconComponent(props: ITaskynIconProps) {
-  const { name, size, color } = props;
+  const { name, size, color, svg } = props;
+  if (name === "google") {
+    return <Google size={size} />;
+  }
+  if (svg) {
+    if (name === "file") {
+      return <File size={size} />;
+    }
+    if (name === "file-history") {
+      return <FileHistory size={size} />;
+    }
+    if (name === "form") {
+      return <Form size={size} />;
+    }
+
+    if (name === "instagram") {
+      return <Instagram size={size} />;
+    }
+    if (name === "note") {
+      return <Note size={size} />;
+    }
+    if (name === "practice") {
+      return <Practice />;
+    }
+    return <Icon name={name} size={size} color={color} />;
+  }
   return <Icon name={name} size={size} color={color} />;
 }
 
