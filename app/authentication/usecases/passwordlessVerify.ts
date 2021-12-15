c574a991ffa9dd247b028e71da8ae393b5ae25ab
eyousefifar <eyousefifar@gmail.com>
2021-12-15 23:34:13 +0330

    fix state names, add mmkv storage

--- app/authentication/usecases/passwordlessVerify.ts
+++ app/authentication/usecases/passwordlessVerify.ts
@@ -1,32 +1,40 @@
 import { secureStorage, storage } from "../../library";
 import { fetchPasswordlessVerify } from "../adapters";
-import { input, auth } from "../entities";
+import { inputState, authState } from "../entities";
 
 export async function passwordlessVerify() {
-  if (!input.otpNumber) {
-    input.setOtpValidation(["کد تایید را وارد کنید"]);
+  if (!inputState.otpNumber) {
+    inputState.setOtpValidation(["کد تایید را وارد کنید"]);
   }
-  const { error, jwt, jwtExpires, refreshExpires, refreshToken, role } =
-    await fetchPasswordlessVerify(input.otpNumber);
+  const { error, jwt, jwtExpires, refreshExpires, refreshToken, role, userId } =
+    await fetchPasswordlessVerify(inputState.otpNumber);
   if (error) {
-    input.setOtpValidation([error]);
+    inputState.setOtpValidation([error]);
     return;
   }
-  if (!jwt || !refreshToken || !refreshExpires || !jwtExpires || !role) {
+  if (
+    !jwt ||
+    !refreshToken ||
+    !refreshExpires ||
+    !jwtExpires ||
+    !role ||
+    !userId
+  ) {
     return;
   }
   await Promise.all([
     secureStorage.add("refresh_token", refreshToken),
     secureStorage.add("token", jwt),
-    storage.add("token_expires_at", `${jwtExpires}`),
-    storage.add("refresh_expires_at", `${refreshExpires}`),
-    storage.add("role", role),
   ]);
-  auth.setRefreshToken(refreshToken);
-  auth.setToken(jwt);
-  auth.setRefreshExpire(refreshExpires);
-  auth.setTokenExpire(jwtExpires);
+  storage.add("token_expires_at", jwtExpires),
+    storage.add("refresh_expires_at", refreshExpires),
+    storage.add("role", role),
+    storage.add("userId", userId);
+  authState.setRefreshToken(refreshToken);
+  authState.setToken(jwt);
+  authState.setRefreshExpire(refreshExpires);
+  authState.setTokenExpire(jwtExpires);
   if (role === "customer" || role === "provider") {
-    auth.setRole(role);
+    authState.setRole(role);
   }
 }
