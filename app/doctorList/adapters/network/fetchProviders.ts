5883ef457c9db6dd808f98beeb3c3561208e053a
eyousefifar <eyousefifar@gmail.com>
2021-12-17 16:38:36 +0330

    parse providers in fetch provider doctor list

--- app/doctorList/adapters/network/fetchProviders.ts
+++ app/doctorList/adapters/network/fetchProviders.ts
@@ -1,5 +1,5 @@
-import { request } from "../../../library";
-import { IFetchProviders } from "../../types";
+import { request, toString, toJalaaliDate } from "../../../library";
+import { IFetchProviders, IUser } from "../../types";
 
 export async function fetchProviders(): Promise<IFetchProviders> {
   const { success, error, httpStatus, payload } = await request({
@@ -8,6 +8,41 @@ export async function fetchProviders(): Promise<IFetchProviders> {
     method: "GET",
   });
   if (!success) {
-    return {};
+    return { error, providers: [] };
   }
+  if (Array.isArray(payload)) {
+    const result: IUser[] = [];
+    for (let index = 0; index < payload.length; index++) {
+      const provider = payload[index];
+      const birthday =
+        Date.parse(provider?.birthday) !== NaN
+          ? toJalaaliDate(new Date(provider?.birthday))
+          : "";
+      const createdAt = new Date(provider?.createdAt);
+      const modifiedAt = new Date(provider?.modifiedAt);
+      result.push({
+        id: toString(provider?.id),
+        role: toString(provider?.role),
+        username: toString(provider?.username),
+        firstName: toString(provider?.firstName),
+        lastName: toString(provider?.lastName),
+        description: toString(provider?.description),
+        gender: provider?.gender === "male" ? "male" : "female",
+        birthday,
+        profilePictureUrl: toString(provider?.profilePictureUrl),
+        address: toString(provider?.address),
+        telephone: toString(provider?.telephone),
+        createdAt,
+        modifiedAt,
+      });
+    }
+    return {
+      error: "",
+      providers: result,
+    };
+  }
+  return {
+    error: "دکتر یافت نشد!",
+    providers: [],
+  };
 }
