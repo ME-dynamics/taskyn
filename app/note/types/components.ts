1122c6f1fa857adc3515fd21dd91d1610903e532
eyousefifar <eyousefifar@gmail.com>
2021-12-18 21:25:16 +0330

    note image type

--- app/note/types/components.ts
+++ app/note/types/components.ts
@@ -1,6 +1,10 @@
-
-
 export interface INoteImageProps {
+  id: string;
+  onRemovePress: (id: string) => void;
+  onImagePress: (id: string) => void;
+}
+
+export interface ICreateNoteImageProps {
   imageId: string;
   path: string;
   onCropPress: (path: string) => void;
