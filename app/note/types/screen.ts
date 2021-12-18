5c2af4038031cd066074761376bd2c50c309d879
eyousefifar <eyousefifar@gmail.com>
2021-12-18 21:25:00 +0330

    scroller ref type

---
+++ app/note/types/screen.ts
@@ -0,0 +1,4 @@
+import { ScrollView } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+export type tScrollRef = ScrollView & KeyboardAwareScrollView;
