cf792b74abc37897c5b8ec0e5d6cf4f3dde6a99d
eyousefifar <eyousefifar@gmail.com>
2021-12-28 20:03:13 +0330

    add ref types

--- app/library/Scroller/types.ts
+++ app/library/Scroller/types.ts
@@ -1,12 +1,19 @@
+import { Ref } from "react";
 import { ScrollViewProps, ViewProps } from "react-native";
-import { KeyboardAwareScrollViewProps } from "react-native-keyboard-aware-scroll-view";
+import { ScrollView } from "react-native-gesture-handler";
+import {
+  KeyboardAwareScrollViewProps,
+  KeyboardAwareScrollView,
+} from "react-native-keyboard-aware-scroll-view";
+
+export type tScrollerRef = ScrollView & KeyboardAwareScrollView;
 
 export interface IScroller
   extends ScrollViewProps,
     KeyboardAwareScrollViewProps {
+  ref: Ref<tScrollerRef>;
   rtl?: boolean;
   keyboard?: boolean;
 }
-
 
 export interface IRTLView extends ViewProps {}
