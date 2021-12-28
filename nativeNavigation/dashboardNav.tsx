505dc16da05774f68bb03facfb332a1318a46d6f
eyousefifar <eyousefifar@gmail.com>
2021-12-28 20:16:21 +0330

    add provider navigation to dashboard, observed

--- nativeNavigation/dashboardNav.tsx
+++ nativeNavigation/dashboardNav.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { observer } from "mobx-react-lite";
 import { DoctorList } from "../app/doctorList";
 import { UserInfo } from "../app/userInfo";
+import { Note } from "../app/note";
 import { Tasks } from "../app/task";
 import { Dashboard } from "../app/dashboard";
+
+import { getRole } from "../app/authentication";
+
 const DashboardStack = createNativeStackNavigator();
 
-export function DashboardNav() {
+function DashboardNavigation() {
+  const role = getRole();
   return (
     <DashboardStack.Navigator initialRouteName={"dashboard"}>
       <DashboardStack.Screen
@@ -17,6 +23,11 @@ export function DashboardNav() {
       <DashboardStack.Screen name={"doctorList"} component={DoctorList} />
       <DashboardStack.Screen name={"userInfo"} component={UserInfo} />
       <DashboardStack.Screen name={"tasks"} component={Tasks} />
+      {role === "provider" ? (
+        <DashboardStack.Screen name={"tasks"} component={Note} />
+      ) : null}
     </DashboardStack.Navigator>
   );
 }
+
+export const DashboardNav = observer(DashboardNavigation);
