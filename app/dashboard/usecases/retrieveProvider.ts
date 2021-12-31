2ba6414edcfdfc048f2b978e90c30c336568c80a
eyousefifar <eyousefifar@gmail.com>
2021-12-31 14:17:06 +0330

    set empty state on doctor error

--- app/dashboard/usecases/retrieveProvider.ts
+++ app/dashboard/usecases/retrieveProvider.ts
@@ -5,6 +5,12 @@ export async function retrieveProvider() {
   const { error, id, name, profilePictureUrl, description } =
     await fetchProviderInfo();
   if (error) {
+    dashboardState.setProvider({
+      id: "",
+      name: "",
+      profilePictureUrl: "",
+      description: "",
+    });
   }
   dashboardState.setProvider({
     id,
