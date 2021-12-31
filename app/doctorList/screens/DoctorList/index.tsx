a6eb0a664407983255badbf46a8f797bf577236c
eyousefifar <eyousefifar@gmail.com>
2021-12-31 14:17:46 +0330

    render doctor list based on state

--- app/doctorList/screens/DoctorList/index.tsx
+++ app/doctorList/screens/DoctorList/index.tsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "react-native";
-import { SearchBar, Title } from "../../../library";
 import { observer } from "mobx-react-lite";
-import { styles } from "./styles";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { useNavigation } from "@react-navigation/core";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { UserList } from "../../components/UserList";
+import { SearchBar, Container, Scroller, Title } from "../../../library";
+import { DoctorCard } from "../../components";
+import { doctorsState } from "../../entities";
+import { retrieveProviders, retrieveRequest } from "../../usecases";
+import { styles } from "./styles";
+
+const image =
+  "https://cdn01.zoomit.ir/2021/12/iran-national-network.jpg?w=1300";
 
 function DoctorListScreen() {
   const navigation = useNavigation<NativeStackNavigationProp<any>>();
-
+  useEffect(() => {
+    async function prepare() {
+      await Promise.all([retrieveProviders(), retrieveRequest()]);
+    }
+    prepare();
+  }, []);
+  function renderDoctorList() {
+    const doctors: JSX.Element[] = [];
+    for (let index = 0; index < doctorsState.doctors.length; index++) {
+      const { id, description, firstName, lastName } =
+        doctorsState.doctors[index];
+      doctors.push(
+        <DoctorCard
+          key={id}
+          id={id}
+          mode={"doctors"}
+          fullName={`${firstName} ${lastName}`}
+          description={description || ""}
+          profileImageUrl={image || ""}
+        />
+      );
+    }
+    return doctors;
+  }
   return (
-    <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Title>{"لیست دکترها"}</Title>
-      </View>
-      <View style={styles.searchBarContainer}>
+    <Container>
+      <View style={styles.searchContainer}>
         <SearchBar
           onChangeText={() => {}}
           // onClear={() => {}}
@@ -24,14 +48,25 @@ function DoctorListScreen() {
           placeholder={""}
         />
       </View>
+      <View style={styles.doctorListContainer}>
+        <Scroller>{renderDoctorList()}</Scroller>
+      </View>
+      <View style={styles.requestContainer}>
+        <View style={styles.horizontalLine} />
+        <Title style={styles.myDoctorTitle}>{"دکتر من"}</Title>
 
-      <KeyboardAwareScrollView style={styles.scrolViewContainer}>
-        <View style={styles.doctorsCardContainer}>
-          <UserList name={"سجاد سیف اله"} mode={"request"} />
-          <UserList name={"سجاد سیف اله"} mode={"request"} />
-        </View>
-      </KeyboardAwareScrollView>
-    </View>
+        <DoctorCard
+          key={doctorsState.myDoctor?.id}
+          mode={"myDoctor"}
+          id={doctorsState.myDoctor?.id || ""}
+          fullName={`${doctorsState.myDoctor?.firstName} ${doctorsState.myDoctor?.lastName}`}
+          description={doctorsState.myDoctor?.description || ""}
+          profileImageUrl={
+            "https://cdn01.zoomit.ir/2021/12/iran-national-network.jpg?w=1300"
+          }
+        />
+      </View>
+    </Container>
   );
 }
 
--- app/doctorList/screens/DoctorList/styles.ts
+++ app/doctorList/screens/DoctorList/styles.ts
@@ -1,78 +1,27 @@
 import { StyleSheet } from "react-native";
-import {
-  heightPercentageToDP,
-  widthPercentageToDP,
-} from "react-native-responsive-screen";
+import { widthPercentageToDP } from "react-native-responsive-screen";
 import { THEME } from "../../../library";
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f5f5f5",
-    alignItems: "center",
-  },
-  scrolViewContainer: {
-    flex: 1,
-  },
-  titleContainer: {
-    width: widthPercentageToDP("100"),
-    height: heightPercentageToDP("10"),
+  searchContainer: {
+    flex: 2,
     alignItems: "center",
     justifyContent: "center",
   },
-
-  searchBarContainer: {
-    width: widthPercentageToDP("100"),
-    height: heightPercentageToDP("10"),
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    flexDirection: "row-reverse",
+  horizontalLine: {
+    width: THEME.WIDTH.WIDE,
+    height: 1,
+    backgroundColor: THEME.COLORS.GREY.LIGHT,
+    alignSelf: "center",
   },
-  doctorsCardContainer: {
-    width: widthPercentageToDP("90"),
-    minHeight: heightPercentageToDP("30"),
-    alignItems: "flex-end",
+  doctorListContainer: {
+    flex: 12,
   },
-  iconContainer: {
-    width: widthPercentageToDP("18"),
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
+  requestContainer: {
+    flex: 4,
+    justifyContent: "space-around",
   },
-  redCircle: {
-    height: 20,
-    width: 20,
-    borderRadius: 20 / 2,
-    backgroundColor: THEME.COLORS.RED.NORMAL,
-  },
-  numberStyle: {
-    textAlign: "center",
-    fontSize: 15,
-    fontWeight: "bold",
-    color: "white",
-  },
-  redCircleContainer: {
-    position: "absolute",
-    top: 10,
-    right: 40,
-    zIndex: 1000,
-  },
-  containerContentStyle: {
-    alignItems: "center",
-  },
-  icon: {
-    width: 40,
-    height: 40,
-    borderRadius: 32 / 2,
-    backgroundColor: "#EEE5FF",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  infoContainer: {
-    flex: 10,
-  },
-  title: {
-    flex: 1,
-    justifyContent: "center",
+  myDoctorTitle: {
+    paddingRight: widthPercentageToDP(6),
   },
 });
