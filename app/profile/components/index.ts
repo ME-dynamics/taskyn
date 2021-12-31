ab549dd7e285a08f20b9d4106ba15485a23d4b68
eyousefifar <eyousefifar@gmail.com>
2021-12-31 14:23:56 +0330

    export profile component

--- app/profile/components/index.ts
+++ app/profile/components/index.ts
@@ -1,1 +1,5 @@
 export { MenuItem } from "./MenuItem";
+export { Avatar } from "./Avatar";
+export { ExitSheet } from "./ExitSheet";
+export { CustomBackdrop } from "./CustomBackdrop";
+export { PickImageSheet } from "./PickImageSheet"
