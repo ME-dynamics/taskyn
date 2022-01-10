1eae9efb55a6410d4f818564e32a31579b5f761c
eyousefifar <eyousefifar@gmail.com>
2022-01-10 13:56:52 +0330

    replace async storage with mmkv

--- android/app/src/main/java/com/abasoft/taskyn/TaskynJSIPackage.java
+++ android/app/src/main/java/com/abasoft/taskyn/TaskynJSIPackage.java
@@ -3,12 +3,15 @@ import com.facebook.react.bridge.JSIModuleSpec;
 import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+import com.reactnativemmkv.MmkvModule;
+
 
 import java.util.List;
 
 public class TaskynJSIPackage extends ReanimatedJSIModulePackage {
     @Override
     public List<JSIModuleSpec> getJSIModules(ReactApplicationContext reactApplicationContext, JavaScriptContextHolder jsContext) {
+        MmkvModule.install(jsContext, reactApplicationContext.getFilesDir().getAbsolutePath() + "/mmkv");
         return super.getJSIModules(reactApplicationContext, jsContext);
     }
 }
