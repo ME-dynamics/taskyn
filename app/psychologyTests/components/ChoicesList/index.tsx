aeaa70b55a77ed7860e950b9e7f533d70bd812b7
eyousefifar <eyousefifar@gmail.com>
2022-01-10 14:00:09 +0330

    move choices list to dedicated component

---
+++ app/psychologyTests/components/ChoicesList/index.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback } from "react";
+import { observer } from "mobx-react-lite";
+import { AnswerCard } from "../AnswerCard";
+import { questionnaireState, testDetailState } from "../../entities";
+import { onSetAnswer } from "../../usecases";
+
+function ChoicesListComponent() {
+  const choices =
+    testDetailState.test.fields[`${questionnaireState.currentQuestion}`]
+      .choices;
+  const answerCards: JSX.Element[] = [];
+  const setAnswer = useCallback(onSetAnswer, [
+    questionnaireState.currentQuestion,
+  ]);
+  for (let index = 0; index < choices.length; index++) {
+    const { label, value } = choices[index];
+    answerCards.push(
+      <AnswerCard
+        key={label}
+        id={`${value}`}
+        selected={
+          questionnaireState.answers[
+            `${questionnaireState.currentQuestion}`
+          ] === `${value}`
+        }
+        text={label}
+        onPress={setAnswer}
+      />
+    );
+  }
+  return <>{answerCards}</>;
+}
+
+export const ChoicesList = observer(ChoicesListComponent);
