d83be11237b947ec28059e6f12a2f53f8d98c848
eyousefifar <eyousefifar@gmail.com>
2022-01-11 13:46:44 +0330

    push to note and create note screen from note list

--- app/note/screens/NoteList/index.tsx
+++ app/note/screens/NoteList/index.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { View, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { observer } from "mobx-react-lite";
 import { Button, Title, TaskynIcon } from "../../../library";
 import { NoteCard } from "../../components/NoteCard";
 import { styles } from "./styles";
 
 function NoteListScreen() {
+  const navigator = useNavigation<NativeStackNavigationProp<any>>();
   const [empty, setEmpty] = useState<boolean>(false);
   return (
     <View style={styles.container}>
@@ -20,7 +23,9 @@ function NoteListScreen() {
         >
           <NoteCard
             id={"1"}
-            onPress={(id) => {}}
+            onPress={(id) => {
+              navigator.push("note");
+            }}
             date={"۱۳۷۶/۰۶/۰۶"}
             description={
               "بیمار از وسواس فکری شدید رنج میبسش تنشس یتسشی مشس ینمشسدیمشس دیم دش رد..ی بتنی دبشبمشسبمش.."
@@ -47,6 +52,9 @@ function NoteListScreen() {
             return <TaskynIcon name={"camera"} size={size} color={color} />;
           }}
           fullRadius
+          onPress={() => {
+            navigator.push("createNote");
+          }}
         >
           {"ایجاد یادداشت جدید"}
         </Button>
