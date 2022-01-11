3ce5ad5c1613546ff80f5016d34d0bd38a23e287
eyousefifar <eyousefifar@gmail.com>
2022-01-11 13:45:42 +0330

    clean up container

--- app/library/Container/index.tsx
+++ app/library/Container/index.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StatusBar } from "expo-status-bar";
 import { observer } from "mobx-react-lite";
 import { NetInfo } from "./NetInfo";
-import { styles, statusBarBackgroundColor } from "./styles";
+import { styles } from "./styles";
 import { IContainerProps } from "./types";
+
 function ContainerComponent(props: IContainerProps) {
   const { children, style } = props;
 
