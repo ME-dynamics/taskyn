094da97a0aa3c6f8a3da2b6abf4889276a9dbd7b
eyousefifar <eyousefifar@gmail.com>
2022-01-14 17:43:20 +0330

    export components

---
+++ app/psychologyTests/components/index.ts
@@ -0,0 +1,8 @@
+export { AnswerCard } from "./AnswerCard";
+export { ProgressBar } from "./ProgressBar";
+export { TestHistoryCard } from "./TestHistoryCard";
+export { TextIcon } from "./TextIcon";
+export { TestCard } from "./TestCard";
+export { QuestionNavigator } from "./QuestionNavigator";
+export { TestHistorySheet } from "./TestHistorySheet"
+export { ChoicesList } from "./ChoicesList";
