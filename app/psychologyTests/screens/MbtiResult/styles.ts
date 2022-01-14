7b63000f95f8ff40973f133c6ac7634e3f57f644
eyousefifar <eyousefifar@gmail.com>
2022-01-14 17:47:46 +0330

    mbti result screen

---
+++ app/psychologyTests/screens/MbtiResult/styles.ts
@@ -0,0 +1,58 @@
+import { StyleSheet } from "react-native";
+import { widthPercentageToDP } from "react-native-responsive-screen";
+import { THEME } from "../../../library";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  progressBarGroupContainer: {
+    width: widthPercentageToDP(100),
+    minHeight: 700,
+  },
+  imageContainer: {
+    width: widthPercentageToDP(100),
+    height: 300,
+    padding: 16,
+  },
+  avatar: {
+    width: "100%",
+    height: "100%",
+  },
+  line: {
+    width: widthPercentageToDP("90"),
+    height: 1,
+    top: 16,
+    backgroundColor: THEME.COLORS.TRANSPARENT.PRIMARY,
+  },
+  answerContainer: {
+    width: widthPercentageToDP(100),
+    height: 62,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  descriptionContainer: {
+    width: widthPercentageToDP(100),
+    minHeight: 50,
+    paddingHorizontal: widthPercentageToDP("5"),
+  },
+  title: {
+    width: widthPercentageToDP(100),
+    height: 30,
+    paddingHorizontal: widthPercentageToDP("5"),
+  },
+  titleBar: {
+    width: widthPercentageToDP(100),
+    height: 70,
+    paddingTop: 24,
+    paddingHorizontal: widthPercentageToDP("5"),
+  },
+  buttonContainer: {
+    width: widthPercentageToDP(100),
+    height: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
