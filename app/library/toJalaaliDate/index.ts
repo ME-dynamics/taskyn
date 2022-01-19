072bcf6a94161a654d533a2ad7da0e22cf34248e
eyousefifar <eyousefifar@gmail.com>
2022-01-19 12:58:09 +0330

    parseDate function to check if date is valid

--- app/library/toJalaaliDate/index.ts
+++ app/library/toJalaaliDate/index.ts
@@ -1,5 +1,14 @@
 import { toJalaali } from "jalaali-js";
 import { digitsEnToFa } from "@persian-tools/persian-tools";
+
+export function parseDate(date: string): Date | undefined {
+  const parsedDate = Date.parse(date);
+  if (isNaN(parsedDate)) {
+    return undefined;
+  }
+  return new Date(parsedDate);
+}
+
 export function toJalaaliDate(date: Date) {
   const { jd, jm, jy } = toJalaali(date);
   return `${digitsEnToFa(jy)}/${digitsEnToFa(jm)}/${digitsEnToFa(jd)}`;
