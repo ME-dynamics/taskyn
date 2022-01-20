c0eea22203f51280aa4372a1dd8531f9997f7dbc
eyousefifar <eyousefifar@gmail.com>
2022-01-20 20:50:03 +0330

    fix note list data and design

--- app/note/screens/NoteList/index.tsx
+++ app/note/screens/NoteList/index.tsx
@@ -1,48 +1,67 @@
-import React, { useState } from "react";
-import { View, ScrollView } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { observer } from "mobx-react-lite";
-import { Button, Title, TaskynIcon } from "../../../library";
+import {
+  Button,
+  Title,
+  TaskynIcon,
+  Container,
+  Scroller,
+} from "../../../library";
+
 import { NoteCard } from "../../components/NoteCard";
+import { retrieveNotes } from "../../usecases";
+
 import { styles } from "./styles";
+import { noteListState } from "../../entities";
 
 function NoteListScreen() {
   const navigator = useNavigation<NativeStackNavigationProp<any>>();
-  const [empty, setEmpty] = useState<boolean>(false);
-  return (
-    <View style={styles.container}>
-      {empty ? (
+  const route = useRoute();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  async function init() {
+    // await retrieveNotes(route?.params?.id);
+    await retrieveNotes("3c959478-8333-4d90-8ede-05d4c9226488");
+    setLoading(false);
+  }
+  useEffect(() => {
+    init();
+  }, []);
+  function renderNotes() {
+    const length = noteListState.notes.length;
+    if (length === 0) {
+      return (
         <View style={styles.empty}>
           <Title>{"شما هنوز یادداشتی ایجاد نکردید."}</Title>
         </View>
-      ) : (
-        <ScrollView
-          style={styles.noteContainer}
-          contentContainerStyle={{ alignItems: "center" }}
-        >
-          <NoteCard
-            id={"1"}
-            onPress={(id) => {
-              navigator.push("note");
-            }}
-            date={"۱۳۷۶/۰۶/۰۶"}
-            description={
-              "بیمار از وسواس فکری شدید رنج میبسش تنشس یتسشی مشس ینمشسدیمشس دیم دش رد..ی بتنی دبشبمشسبمش.."
-            }
-            title={"وسواس فکری"}
-          />
-          <NoteCard
-            id={"1"}
-            onPress={(id) => {}}
-            date={"۱۳۷۶/۰۶/۰۶"}
-            description={
-              "بیمار از وسواس فکری شدید رنج میبسش تنشس یتسشی مشس ینمشسدیمشس دیم دش رد..ی بتنی دبشبمشسبمش.."
-            }
-            title={"وسواس فکری"}
-          />
-        </ScrollView>
-      )}
+      );
+    }
+    const components: JSX.Element[] = [];
+    for (let index = 0; index < length; index++) {
+      const { id, content, createdAt, title } = noteListState.notes[index];
+      components.push(
+        <NoteCard
+          key={id}
+          id={id}
+          onPress={(id) => {
+            console.log(id);
+          }}
+          title={title}
+          date={createdAt}
+          description={content}
+        />
+      );
+    }
+    return components;
+  }
+  return (
+    <Container loading={loading}>
+      <Scroller contentContainerStyle={styles.scrollerContainerView}>
+        {renderNotes()}
+      </Scroller>
       <View style={styles.buttonContainer}>
         <Button
           mode={"contained"}
@@ -59,7 +78,7 @@ function NoteListScreen() {
           {"ایجاد یادداشت جدید"}
         </Button>
       </View>
-    </View>
+    </Container>
   );
 }
 
