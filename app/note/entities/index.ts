329f001f876b674d97d8dbe2b0559a46bca33a9d
eyousefifar <eyousefifar@gmail.com>
2022-01-20 20:49:43 +0330

    export new states , remove old state

--- app/note/entities/index.ts
+++ app/note/entities/index.ts
@@ -1,2 +1,5 @@
-import { NoteState } from "./note";
-export const noteState = new NoteState();
+import { NoteListState } from "./noteList";
+import { CreateNoteState } from "./createNote";
+
+export const noteListState = new NoteListState();
+export const createNoteState = new CreateNoteState();
