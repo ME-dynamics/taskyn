3738dfef0a31f0e6769dfdba701a855fd71b6422
eyousefifar <eyousefifar@gmail.com>
2022-01-24 14:08:01 +0330

    implementation psychologyTests entities

--- app/psychologyTests/entities/index.ts
+++ app/psychologyTests/entities/index.ts
@@ -1,7 +1,9 @@
 import { TestListState } from "./testList";
 import { TestDetailState } from "./testDetail";
 import { QuestionnaireState } from "./questionnaire";
+import { MbtiState } from "./mbti";
 
 export const testListState = new TestListState();
 export const testDetailState = new TestDetailState();
 export const questionnaireState = new QuestionnaireState();
+export const mbtiState = new MbtiState();
