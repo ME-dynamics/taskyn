8d476ec4d44f5e6dc71752e483bd988a9d89f40b
eyousefifar <eyousefifar@gmail.com>
2022-01-25 13:49:24 +0330

    replace state with noteList current note state

--- app/note/screens/Note/index.tsx
+++ app/note/screens/Note/index.tsx
@@ -1,19 +1,31 @@
 import React, { useRef, useMemo } from "react";
 import { View } from "react-native";
 import Gallery from "react-native-awesome-gallery";
+import { useRoute } from "@react-navigation/native";
 import {
   BottomSheetModalProvider,
   BottomSheetModal,
   BottomSheetBackdrop,
 } from "@gorhom/bottom-sheet";
 import { observer } from "mobx-react-lite";
+
 import { Container, Scroller, Input } from "../../../library";
 import { NoteImage, NoteHeader, DeleteSheet } from "../../components";
+import { noteListState } from "../../entities";
+import { updateNote } from "../../usecases";
 
 import { styles } from "./styles";
+
 function NoteScreen() {
+  const route = useRoute();
+  
   const bottomSheetRef = useRef<BottomSheetModal>(null);
   const snapPoints = useMemo(() => ["30%", "30%"], []);
+  async function onUpdateNote() {
+    if (noteListState.currentNote) {
+      await updateNote(noteListState.currentNote);
+    }
+  }
   function onDeletePress() {
     bottomSheetRef.current?.present();
   }
@@ -26,7 +38,10 @@ function NoteScreen() {
   return (
     <BottomSheetModalProvider>
       <Container>
-        <NoteHeader onDeletePress={onDeletePress} />
+        <NoteHeader
+          onUpdateNotePress={onUpdateNote}
+          onDeletePress={onDeletePress}
+        />
         <Scroller keyboard>
           {/* header */}
           <View style={styles.titleInputContainer}>
@@ -34,6 +49,10 @@ function NoteScreen() {
               mode={"raw"}
               title={"موضوع"}
               placeholder={"موضوع خود را وارد کنید"}
+              value={noteListState.currentNote?.title}
+              onChangeText={(text) => {
+                noteListState.setCurrentNoteTitle(text);
+              }}
             />
             <View style={styles.horizontalLine} />
           </View>
@@ -43,6 +62,10 @@ function NoteScreen() {
               title={"متن"}
               placeholder={"موضوع خود را وارد کنید"}
               multiline
+              value={noteListState.currentNote?.content}
+              onChangeText={(text) => {
+                noteListState.setCurrentNoteContent(text);
+              }}
             />
           </View>
           <NoteImage
