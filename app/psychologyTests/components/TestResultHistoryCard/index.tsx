a84d31238dffedd2172807649abdd5cc0ba0c160
eyousefifar <eyousefifar@gmail.com>
2022-01-26 20:20:25 +0330

    fix testResu;tHistoryCard styles and change answer prop to description

--- app/psychologyTests/components/TestResultHistoryCard/index.tsx
+++ app/psychologyTests/components/TestResultHistoryCard/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { View } from "react-native";
-import { observer } from "mobx-react-lite"
+import { observer } from "mobx-react-lite";
 import { Touchable, Paragraph, Subheading, Caption } from "../../../library";
 import { ITestResultHistoryCardProps } from "../../types";
 import { styles } from "./styles";
 function TestResultHistoryCardComponent(props: ITestResultHistoryCardProps) {
-  const { Icon, title, answer, onPress, date, id } = props;
+  const { Icon, title, description, onPress, date, id } = props;
   function onCardPress() {
     if (onPress) {
       onPress(id);
@@ -17,14 +17,14 @@ function TestResultHistoryCardComponent(props: ITestResultHistoryCardProps) {
         <Icon />
       </View>
       <View style={styles.infoContainer}>
-        <View style={styles.descriptionContainer}>
+        <View style={styles.testDetailsContainer}>
           <View style={styles.titleContainer}>
             <Subheading
               style={styles.textAlignRight}
             >{`تست ${title}`}</Subheading>
           </View>
-          <View style={styles.answerContainer}>
-            <Paragraph style={styles.answer}>{`نتیجه: ${answer}`}</Paragraph>
+          <View style={styles.descriptionContainer}>
+            <Paragraph style={styles.description}>{description}</Paragraph>
           </View>
         </View>
         <View style={styles.dateContainer}>
@@ -34,7 +34,6 @@ function TestResultHistoryCardComponent(props: ITestResultHistoryCardProps) {
       <Touchable onPress={onCardPress} rippleColor={"grey"} />
     </View>
   );
-};
-
+}
 
 export const TestResultHistoryCard = observer(TestResultHistoryCardComponent);
