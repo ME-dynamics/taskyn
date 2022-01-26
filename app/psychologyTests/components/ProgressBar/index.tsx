160dce79b8f3d4f15e24828b575a825881fb3648
eyousefifar <eyousefifar@gmail.com>
2022-01-26 20:19:05 +0330

    fix progressBar styles

--- app/psychologyTests/components/ProgressBar/index.tsx
+++ app/psychologyTests/components/ProgressBar/index.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { View } from "react-native";
-import { Paragraph, THEME, Title } from "../../../library";
+import { Paragraph, THEME } from "../../../library";
 import { IProgressProps } from "../../types";
 import { styles } from "./styles";
 export function ProgressBar(props: IProgressProps) {
   const { leftName, leftPercent, rightName } = props;
-  // function rightPercent(arg: number){
-  //   const value = (100-leftPercent)/100;
-  //   return value;
-  // }
   const rightPercent = 100 - leftPercent;
   const leftStyle = {
     flex: leftPercent,
@@ -18,34 +14,23 @@ export function ProgressBar(props: IProgressProps) {
   };
   return (
     <View style={styles.container}>
-      {/* <View style={styles.progressTitle}>
-        <Title>{title}</Title>
-      </View> */}
-      {/* <View style={styles.progressDescription}>
-        <Paragraph style={{ color: "#727272" }}>{description}</Paragraph>
-      </View> */}
       <View style={styles.progressBar}>
         <View style={[styles.progressLeft, leftStyle]}>
-          <View style={styles.titleWithPercent}>
-            <Paragraph style={{ color: "white" }}>{leftName}</Paragraph>
-            <Paragraph
-              style={{ color: "white", left: 6 }}
-            >{`${leftPercent}%`}</Paragraph>
-          </View>
+          <Paragraph style={{ color: "white" }}>{`${leftPercent}%`}</Paragraph>
         </View>
         <View style={[styles.progressRight, rightStyle]}>
-          <View style={styles.titleWithPercent}>
-            <Paragraph style={{ color: THEME.COLORS.PRIMARY.NORMAL, right: 6 }}>
-              {rightName}
-            </Paragraph>
-            <Paragraph
-              style={{ color: THEME.COLORS.PRIMARY.NORMAL }}
-            >{`${rightPercent}%`}</Paragraph>
-          </View>
+          <Paragraph
+            style={{ color: THEME.COLORS.PRIMARY.NORMAL }}
+          >{`${rightPercent}%`}</Paragraph>
         </View>
       </View>
-      <View style={styles.typeTitle}></View>
-      <View style={styles.fullLine} />
+      <View style={styles.titles}>
+        <Paragraph style={{ color: "black" }}>{leftName}</Paragraph>
+        <Paragraph style={{ color: THEME.COLORS.PRIMARY.NORMAL }}>
+          {rightName}
+        </Paragraph>
+      </View>
+      <View style={styles.fullLine}></View>
     </View>
   );
 }
