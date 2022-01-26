76e40dac18a1b58888714af735808d5d4de4dcc8
eyousefifar <eyousefifar@gmail.com>
2022-01-26 20:21:53 +0330

    fix bottomSheet modal in  testDetails

--- app/psychologyTests/screens/TestDetails/index.tsx
+++ app/psychologyTests/screens/TestDetails/index.tsx
@@ -3,7 +3,7 @@ import { View, Image } from "react-native";
 import { observer } from "mobx-react-lite";
 import { useNavigation, useRoute, RouteProp } from "@react-navigation/core";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import BottomSheet from "@gorhom/bottom-sheet";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import {
   Button,
   Paragraph,
@@ -31,11 +31,13 @@ type RouteParams = RouteProp<TestStackParamList, "testDetails">;
 export function TestDetailsScreen() {
   const route = useRoute<RouteParams>();
   const navigator = useNavigation<NativeStackNavigationProp<any>>();
-  const bottomSheetRef = useRef<BottomSheet>(null);
-  const snapPoints = useMemo(() => ["30%", "30%"], []);
-
+  const BottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const snapPoints = useMemo(() => ["40%", "40%"], []);
+  const close = useCallback(() => {
+    BottomSheetModalRef.current?.close();
+  }, []);
   const onCollapsePress = useCallback(() => {
-    bottomSheetRef.current?.collapse();
+    BottomSheetModalRef.current?.present();
   }, []);
   function navigateToQuestionnaire() {
     navigator.push("questionnaire", { id: route.params.id });
@@ -56,72 +58,78 @@ export function TestDetailsScreen() {
   }, []);
   return (
     <Container>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={require("../../assets/Neo.png")}
-          resizeMode={"stretch"}
-        />
-      </View>
-      <View style={styles.detailsContainer}>
-        <Title>{testDetailState.test.title.fa}</Title>
-        <Subheading style={styles.enTitle}>
-          {testDetailState.test.title.en}
-        </Subheading>
-      </View>
-      <View style={styles.descriptionContainer}>
-        <Scroller
-          gestureScroll
-          contentContainerStyle={styles.descriptionScrollViewContent}
-        >
-          <Paragraph>{testDetailState.test.description}</Paragraph>
-        </Scroller>
-      </View>
-      <View style={styles.formDetails}>
-        <View style={styles.formDetailsBody}>
-          <Paragraph style={styles.timeNeeded}>
-            {"زمان لازم: "} <Paragraph>{"۱۰ دقیقه"}</Paragraph>
-          </Paragraph>
-          <TaskynIcon
-            name={"timer"}
-            size={iconStyle.size}
-            color={iconStyle.color}
+      <Scroller>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={require("../Constant/MBTI.png")}
+            resizeMode={"contain"}
           />
         </View>
-        <View style={styles.formDetailsBody}>
-          <Paragraph style={styles.testCount}>
-            {"تعداد تست ها: "}
-            <Paragraph>
-              {Object.keys(testDetailState.test.fields).length}
+        <View style={styles.detailsContainer}>
+          <Title>{testDetailState.test.title.fa}</Title>
+          <Subheading style={styles.enTitle}>
+            {testDetailState.test.title.en}
+          </Subheading>
+        </View>
+        <View style={styles.descriptionContainer}>
+          {/*  get from server */}
+          {/* <Paragraph>{testDetailState.test.description}</Paragraph> */}
+          <Paragraph>
+            {
+              "در نظریه یونگ، هشت الگوی رفتاری انسان معرفی شد. بریگز و مایرز ایده‌های یونگ را در مورد این الگوهای ذهنی توسعه دادند و در نهایت نقش آنها را در مفهوم و توصیف انواع شخصیت‌ها در تست MBTI گنجاندند. تست بریگز و مایرز نوعی ارزیابی است که در آن شخصیت فرد را با توجه به چهار معیار اصلی مورد بررسی قرار می‌گیرد که در آن هر حرف مربوط به یک اولویت یا گرایش شخصیتی خاص است. این معیارها برون گرا یا درون گرا بودن، شهودی یا حسی بوده، ادراکی یا قضاوتی بودن و در نهایت احساسی یا منطقی بودن هستند."
+            }
+          </Paragraph>
+        </View>
+        <View style={styles.formDetails}>
+          <View style={styles.formDetailsBody}>
+            <Paragraph style={styles.timeNeeded}>
+              {"زمان لازم: "} <Paragraph>{"۱۰ دقیقه"}</Paragraph>
             </Paragraph>
-          </Paragraph>
-          <TaskynIcon
-            name={"task"}
-            size={iconStyle.size}
-            color={iconStyle.color}
-          />
+            <TaskynIcon
+              name={"timer"}
+              size={iconStyle.size}
+              color={iconStyle.color}
+            />
+          </View>
+          <View style={styles.formDetailsBody}>
+            <Paragraph style={styles.testCount}>
+              {"تعداد تست ها: "}
+              <Paragraph>
+                {Object.keys(testDetailState.test.fields).length}
+              </Paragraph>
+            </Paragraph>
+            <TaskynIcon
+              name={"task"}
+              size={iconStyle.size}
+              color={iconStyle.color}
+            />
+          </View>
         </View>
-      </View>
-      <View style={styles.startTestContainer}>
-        <Button
-          mode={"contained"}
-          size={"wide"}
-          rippleColor={"lightGrey"}
-          onPress={onStartTestPress}
+        <View style={styles.startTestContainer}>
+          <Button
+            mode={"contained"}
+            size={"wide"}
+            rippleColor={"lightGrey"}
+            onPress={onStartTestPress}
+          >
+            {"شروع تست"}
+          </Button>
+        </View>
+        <BottomSheetModal
+          ref={BottomSheetModalRef}
+          index={1}
+          snapPoints={snapPoints}
+          // onChange={handleSheetChanges}
+          backdropComponent={CustomBackdrop}
+          enablePanDownToClose={true}
         >
-          {"شروع تست"}
-        </Button>
-      </View>
-      <BottomSheet
-        ref={bottomSheetRef}
-        index={-1}
-        snapPoints={snapPoints}
-        // onChange={handleSheetChanges}
-        backdropComponent={CustomBackdrop}
-        enablePanDownToClose={true}
-      >
-        <TestHistorySheet navigateToQuestionnaire={navigateToQuestionnaire} />
-      </BottomSheet>
+          <TestHistorySheet
+            navigateToQuestionnaire={navigateToQuestionnaire}
+            onClosePress={close}
+          />
+        </BottomSheetModal>
+      </Scroller>
     </Container>
   );
 }
