5df8e89f9d7eae3dd61ec7bbab695f10bc0f09ac
eyousefifar <eyousefifar@gmail.com>
2022-01-26 23:02:38 +0330

    customer events clean up

--- app/library/events/customers.ts
+++ app/library/events/customers.ts
@@ -1,12 +1,11 @@
-import { events } from "./eventSingleton"
+import { events } from "./eventSingleton";
 
-
+const RETRIEVE_CUSTOMERS_EVENT = "RETRIEVE_CUSTOMERS_EVENT";
 
 export function retrieveCustomersEvent() {
-    events.emit("retrieveCustomers");
+  events.emit(RETRIEVE_CUSTOMERS_EVENT);
 }
 
-
-export function onRetrieveCustomersEvent(callback: (event: any) => void) {
-    return events.on("retrieveCustomers", callback);
+export function registerRetrieveCustomersEvents(callback: any) {
+  events.on(RETRIEVE_CUSTOMERS_EVENT, callback);
 }
