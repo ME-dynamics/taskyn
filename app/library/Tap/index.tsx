758f9064820e452b3e162dbd611037d0ff47459d
eyousefifar <eyousefifar@gmail.com>
2022-02-18 00:13:18 +0330

    use gesture detector instead of tap, more stable

--- app/library/Tap/index.tsx
+++ app/library/Tap/index.tsx
@@ -1,34 +1,29 @@
 import React from "react";
 import { View } from "react-native";
 import { observer } from "mobx-react-lite";
-import {
-  State,
-  TapGestureHandler,
-  TapGestureHandlerGestureEvent,
-} from "react-native-gesture-handler";
+import { Gesture, GestureDetector, State } from "react-native-gesture-handler";
 import { useDoublePress } from "../doublePress";
 import { ITapProps } from "./types";
 
 function TapComponent(props: ITapProps) {
   const { children, onPress, waitFor } = props;
-  const { onTouchablePress } = useDoublePress(onTapPress);
-  function onTapPress(event: TapGestureHandlerGestureEvent) {
+  function onTapPress() {
     // WHY: after adding useDoublePress, state does not activate
     if (onPress) {
       onPress();
     }
   }
+  const { onTouchablePress } = useDoublePress(onTapPress);
+
+  const tap = Gesture.Tap().onStart(onTouchablePress);
   return (
-    <TapGestureHandler
-      onHandlerStateChange={onTouchablePress}
-      waitFor={waitFor}
-    >
+    <GestureDetector gesture={tap}>
       {
         // there must be a view around children to avoid ref bug
         // that causes crash
       }
       <View>{children}</View>
-    </TapGestureHandler>
+    </GestureDetector>
   );
 }
 
