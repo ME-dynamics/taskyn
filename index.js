dad84ca95af15c7c667f46211f13b4072e322642
eyousefifar <eyousefifar@gmail.com>
2022-02-23 20:03:45 +0330

    add react native ger random values

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
-
+import "react-native-get-random-values";
 import { App } from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
