80b6929aff4a3893f82222e43d8f8750bbe8246a
eyousefifar <eyousefifar@gmail.com>
2022-02-25 21:40:30 +0330

    to jalaali date better parsing

--- app/library/toJalaaliDate/index.ts
+++ app/library/toJalaaliDate/index.ts
@@ -2,8 +2,8 @@ import { toJalaali } from "jalaali-js";
 import { digitsEnToFa } from "@persian-tools/persian-tools";
 
 export function parseDate(date: string): Date | undefined {
-  const parsedDate = Date.parse(date);
-  if (isNaN(parsedDate)) {
+  const parsedDate = new Date(date);
+  if (isNaN(parsedDate.getTime())) {
     return undefined;
   }
   return new Date(parsedDate);
@@ -11,5 +11,5 @@ export function parseDate(date: string): Date | undefined {
 
 export function toJalaaliDate(date: Date) {
   const { jd, jm, jy } = toJalaali(date);
-  return `${digitsEnToFa(jy)}/${digitsEnToFa(jm)}/${digitsEnToFa(jd)}`;
+  return `${digitsEnToFa(jy)}-${digitsEnToFa(jm)}-${digitsEnToFa(jd)}`;
 }
