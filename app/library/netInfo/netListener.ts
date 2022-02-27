55549f5149ee1a7b9ba21019471fa6a563397f87
eyousefifar <eyousefifar@gmail.com>
2022-02-27 19:11:08 +0330

    comment out netInfo configuration

--- app/library/netInfo/netListener.ts
+++ app/library/netInfo/netListener.ts
@@ -1,15 +1,15 @@
 import NetInfo from "@react-native-community/netinfo";
 import { netInfoState } from "./netInfoState";
 // use net info
-NetInfo.configure({
-  // reachabilityUrl: 'https://clients3.google.com/generate_204',
-  // reachabilityTest: async (response) => response.status === 204,
-  // reachabilityLongTimeout: 60 * 1000, // 60s
-  // reachabilityShortTimeout: 5 * 1000, // 5s
-  // reachabilityRequestTimeout: 15 * 1000, // 15s
-  // reachabilityShouldRun: () => true,
-  shouldFetchWiFiSSID: false // met iOS requirements to get SSID. Will leak memory if set to true without meeting requirements.
-});
+// NetInfo.configure({
+//   // reachabilityUrl: 'https://clients3.google.com/generate_204',
+//   // reachabilityTest: async (response) => response.status === 204,
+//   // reachabilityLongTimeout: 60 * 1000, // 60s
+//   // reachabilityShortTimeout: 5 * 1000, // 5s
+//   // reachabilityRequestTimeout: 15 * 1000, // 15s
+//   // reachabilityShouldRun: () => true,
+//   shouldFetchWiFiSSID: false // met iOS requirements to get SSID. Will leak memory if set to true without meeting requirements.
+// });
 
 export function registerNetInfo() {
   const unsubscribe = NetInfo.addEventListener(function stateListener(state) {
