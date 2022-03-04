e673142dfe6093461db5ca01ae8fec59ecdccfd6
eyousefifar <eyousefifar@gmail.com>
2022-03-04 18:17:12 +0330

    fix continue test session

--- app/psychologyTests/usecases/continueTestSession.ts
+++ app/psychologyTests/usecases/continueTestSession.ts
@@ -3,7 +3,10 @@ import { questionnaireState } from "../entities";
 
 export function continueTestSession(testId: string, fieldSize: number) {
   for (let index = 1; index < fieldSize; index++) {
-    const answer = storage.retrieve(`${testId}@${index}`, "string");
+    const key = `${testId}@${index}`;
+    const keyExists = storage.has(key);
+    if (!keyExists) continue;
+    const answer = storage.retrieve(key, "number");
     if (typeof answer === "number") {
       questionnaireState.setAnswer(index, answer);
     }
