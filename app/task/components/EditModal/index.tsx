23bf9eadd391cbaed2731c06a75f7052982dd998
eyousefifar <eyousefifar@gmail.com>
2022-03-04 18:18:13 +0330

    add edit mode to edit modal

--- app/task/components/EditModal/index.tsx
+++ app/task/components/EditModal/index.tsx
@@ -1,24 +1,41 @@
 import React, { useState } from "react";
 import { View } from "react-native";
 import { BottomSheetView } from "@gorhom/bottom-sheet";
+import { toJS } from "mobx";
 import { observer } from "mobx-react-lite";
+import { Button, Input } from "../../../library";
+
+import { taskState } from "../../entities";
+import { updateTask, createTask } from "../../usecases";
 
 import { styles } from "./styles";
-import { Button, Input } from "../../../library";
 import { IEditModalProps } from "../../types";
-import { addEmptyTask, createTask } from "../../usecases";
 
 function EditModalComponent(props: IEditModalProps) {
   const { onClosePress, customerId } = props; // TODO: on close press is right
+  const currentEditTask = toJS(taskState.currentEditTask);
   const [upsertTaskLoading, setUpsertTaskLoading] = useState<boolean>(false);
-  const [edit, setEdit] = useState<boolean>(false); // TODO: move to mobx - task state
-  const [tempContent, setTempContent] = useState("");
+  const [tempContent, setTempContent] = useState(
+    currentEditTask ? currentEditTask.content : ""
+  );
 
   async function onCreatePress() {
+    if (currentEditTask) {
+      const { id } = currentEditTask;
+      setUpsertTaskLoading(true);
+      await updateTask({
+        taskId: id,
+        content: tempContent,
+        userId: customerId,
+      });
+      // setUpsertTaskLoading(false);
+      onClosePress();
+      return;
+    }
     setUpsertTaskLoading(true);
     const created = await createTask(tempContent, customerId || "");
     if (created) {
-      setEdit(false);
+      // setEdit(false);
       // TODO: handle error state here
     }
     setUpsertTaskLoading(false);
@@ -29,14 +46,14 @@ function EditModalComponent(props: IEditModalProps) {
       <Input
         value={tempContent}
         mode={"no-style"}
-        style={{paddingRight:10,}}
+        style={{ paddingRight: 10 }}
         placeholder={"موضوع تمرین را بنویسید ..."}
         multiline={true}
         onChangeText={(text) => setTempContent(text)}
       />
       <View style={styles.buttonContainer}>
         <Button
-          mode={"contained"}
+          mode={"contained-grey"}
           size={"small"}
           rippleColor={"lightGrey"}
           onPress={onClosePress}
