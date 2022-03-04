6d8c50ac16f7fb2548bd45c3af3c8cec2fb11373
eyousefifar <eyousefifar@gmail.com>
2022-03-04 18:18:33 +0330

    remove tesk Item edit mode

--- app/task/components/TaskItem/index.tsx
+++ app/task/components/TaskItem/index.tsx
@@ -8,27 +8,29 @@ import {
   IconButton,
   Paragraph,
 } from "../../../library";
-import { taskDone, updateTask, removeTask, createTask } from "../../usecases";
+
+import { taskState } from "../../entities";
+import { taskDone, removeTask } from "../../usecases";
 import { TaskMenu } from "../TaskMenu";
-import { styles, selectionColor, menuColor } from "./styles";
+
+import { styles, menuColor } from "./styles";
 import { ITaskItemProps } from "../../types";
 
 function TaskItemComponent(props: ITaskItemProps) {
-  const { id, userId, content, done, createdAt } = props;
-  const isEmpty = !createdAt;
+  const { id, userId, content, done, createdAt, showEditModal } = props;
+  // const isEmpty = !createdAt;
   const [showMenu, setShowMenu] = useState(false);
 
-  const [edit, setEdit] = useState<boolean>(isEmpty);
+  // const [edit, setEdit] = useState<boolean>(isEmpty);
   const [doneLoading, setDoneLoading] = useState<boolean>(false);
-  const [upsertTaskLoading, setUpsertTaskLoading] = useState<boolean>(false);
+  // const [upsertTaskLoading, setUpsertTaskLoading] = useState<boolean>(false);
   const [tempContent, setTempContent] = useState(content);
-  const inputRef = useRef<TextInput>(null);
+  // const inputRef = useRef<TextInput>(null);
   function onEditPress() {
     setShowMenu(false);
-    setEdit(true);
-    if (inputRef?.current) {
-      inputRef.current.focus();
-    }
+    taskState.setCurrentEditTask(id);
+    // show edit modal
+    showEditModal();
   }
   async function onRemovePress() {
     await removeTask(id, userId || "");
@@ -41,51 +43,51 @@ function TaskItemComponent(props: ITaskItemProps) {
     } catch (error) {}
   }
   // TODO: move update to edit modal
-  async function onUpdatePress() {
-    setUpsertTaskLoading(true);
-    await updateTask({ taskId: id, content: tempContent, userId });
-    setUpsertTaskLoading(false);
-    setEdit(false);
-  }
+  // async function onUpdatePress() {
+  //   setUpsertTaskLoading(true);
+  //   await updateTask({ taskId: id, content: tempContent, userId });
+  //   setUpsertTaskLoading(false);
+  //   // setEdit(false);
+  // }
   // TODO: remvove create task, empty task
-  async function onCreatePress() {
-    setUpsertTaskLoading(true);
-    const created = await createTask(tempContent, userId || "");
-    if (created) {
-      setEdit(false);
-      // TODO: handle error state here
-    }
-    setUpsertTaskLoading(false);
-  }
+  // async function onCreatePress() {
+  //   setUpsertTaskLoading(true);
+  //   const created = await createTask(tempContent, userId || "");
+  //   if (created) {
+  //     // setEdit(false);
+  //     // TODO: handle error state here
+  //   }
+  //   setUpsertTaskLoading(false);
+  // }
 
   function renderFooter() {
-    if (edit) {
-      if (content) {
-        return (
-          <Button
-            mode={"contained"}
-            size={"big"}
-            loading={upsertTaskLoading}
-            rippleColor={"lightGrey"}
-            onPress={onUpdatePress}
-          >
-            {"بروزرسانی تمرین"}
-          </Button>
-        );
-      } else {
-        return (
-          <Button
-            mode={"contained"}
-            size={"big"}
-            loading={upsertTaskLoading}
-            rippleColor={"lightGrey"}
-            onPress={onCreatePress}
-          >
-            {"اضافه کردن تمرین"}
-          </Button>
-        );
-      }
-    }
+    // if (edit) {
+    //   if (content) {
+    //     return (
+    //       <Button
+    //         mode={"contained"}
+    //         size={"big"}
+    //         loading={upsertTaskLoading}
+    //         rippleColor={"lightGrey"}
+    //         onPress={onUpdatePress}
+    //       >
+    //         {"بروزرسانی تمرین"}
+    //       </Button>
+    //     );
+    //   } else {
+    //     return (
+    //       <Button
+    //         mode={"contained"}
+    //         size={"big"}
+    //         loading={upsertTaskLoading}
+    //         rippleColor={"lightGrey"}
+    //         onPress={onCreatePress}
+    //       >
+    //         {"اضافه کردن تمرین"}
+    //       </Button>
+    //     );
+    //   }
+    // }
     if (done) {
       return <Paragraph style={styles.doneParagraph}>{"انجام دادم"}</Paragraph>;
     }
@@ -104,46 +106,45 @@ function TaskItemComponent(props: ITaskItemProps) {
     );
   }
   return (
-    <>
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <IconButton
-            color={menuColor}
-            onPress={() => {
-              setShowMenu(true);
-            }}
-            Icon={({ size, color }) => {
-              return <TaskynIcon name={"menu"} size={size} color={color} />;
-            }}
-          />
-          <Caption>{createdAt}</Caption>
-        </View>
-        <View style={styles.textContainer}>
-          <TextInput
-            ref={inputRef}
-            style={styles.textInput}
-            onChangeText={setTempContent}
-            placeholder={"تمرین خود را وارد کنید ..."}
-            value={tempContent}
-            multiline={true}
-            selectionColor={selectionColor}
-            editable={edit}
-          />
-        </View>
-        <View style={styles.footer}>{renderFooter()}</View>
-        {showMenu ? (
-          <TaskMenu
-            show={showMenu}
-            onHidePress={() => {
-              setShowMenu(false);
-            }}
-            onEditPress={onEditPress}
-            onRemovePress={onRemovePress}
-          />
-        ) : null}
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <IconButton
+          color={menuColor}
+          onPress={() => {
+            setShowMenu(true);
+          }}
+          Icon={({ size, color }) => {
+            return <TaskynIcon name={"menu"} size={size} color={color} />;
+          }}
+        />
+        <Caption>{createdAt}</Caption>
       </View>
-      {isEmpty ? <View style={{ width: "100%", height: 84 }} /> : null}
-    </>
+      <View style={styles.textContainer}>
+        <Paragraph
+        // ref={inputRef}
+        // style={styles.textInput}
+        // onChangeText={setTempContent}
+        // placeholder={"تمرین خود را وارد کنید ..."}
+        // value={tempContent}
+        // multiline={true}
+        // selectionColor={selectionColor}
+        // editable={edit}
+        >
+          {content}
+        </Paragraph>
+      </View>
+      <View style={styles.footer}>{renderFooter()}</View>
+      {showMenu ? (
+        <TaskMenu
+          show={showMenu}
+          onHidePress={() => {
+            setShowMenu(false);
+          }}
+          onEditPress={onEditPress}
+          onRemovePress={onRemovePress}
+        />
+      ) : null}
+    </View>
   );
 }
 
