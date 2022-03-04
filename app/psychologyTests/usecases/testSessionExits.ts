bace8046532758999299b366cc244994df94e5a7
eyousefifar <eyousefifar@gmail.com>
2022-03-04 18:17:42 +0330

    use has method to check if key exists test session

--- app/psychologyTests/usecases/testSessionExits.ts
+++ app/psychologyTests/usecases/testSessionExits.ts
@@ -1,6 +1,5 @@
 import { storage } from "../../library";
 
 export function testSessionExists(testId: string) {
-  const firstTest = storage.retrieve(`${testId}@1`, "string");
-  return !!firstTest;
+  return storage.has(`${testId}@1`);
 }
