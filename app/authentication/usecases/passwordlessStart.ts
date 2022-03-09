bc49fb35dfad5beef414d3dce8569ebb774dc448
eyousefifar <eyousefifar@gmail.com>
2022-03-09 20:26:17 +0330

    refactor passwordless start and verify

--- app/authentication/usecases/passwordlessStart.ts
+++ app/authentication/usecases/passwordlessStart.ts
@@ -1,27 +1,68 @@
-import { fetchPasswordlessStart, validatePhoneNumber } from "../adapters";
+import { Platform } from "react-native";
+import * as Device from "expo-device";
+import { nanoid } from "nanoid";
+import { storage, logger, toString } from "../../library";
+
+import { fetchPasswordlessStart } from "../adapters";
 import { inputState, authState } from "../entities";
-import { storage } from "../../library";
 export async function passwordlessStart() {
-  if (inputState.phoneNumber && inputState.phoneNumber.length >= 10) {
-    const isPhoneValid = validatePhoneNumber(inputState.phoneNumber);
-    if (!isPhoneValid) {
-      inputState.setPhoneValidation(["شماره تماس اشتباه است."]);
-    } else {
-      inputState.setPhoneValidation([]);
-    }
-    if (isPhoneValid) {
-      const { otpToken, error, deviceId } = await fetchPasswordlessStart(
-        inputState.phoneNumber
-      );
-      if (error) {
-        inputState.setPhoneValidation([error]);
-        return;
-      }
-      if (otpToken) {
-        authState.setOtpToken(otpToken);
-        storage.add("device_id", deviceId);
-        return;
-      }
-    }
+  if (!inputState.isPhoneValid) {
+    logger({
+      container: "authnz",
+      path: { section: "usecases", file: "passwordlessStart" },
+      type: "warn",
+      logMessage:
+        "passwordlessStart called but phone is not valid. " +
+        "this should not happen, button must be disabled when phone is not valid",
+    });
+    return;
   }
+  const uniqueId = toString(storage.retrieve("device_unique_id", "string"));
+
+  const deviceUniqueId = uniqueId || nanoid(64);
+  if (!uniqueId) {
+    storage.add("device_unique_id", deviceUniqueId);
+    logger({
+      container: "authnz",
+      path: { section: "usecases", file: "passwordlessStart" },
+      type: "info",
+      logMessage: "added new unique id to storage",
+    });
+  }
+  const { otpToken, error, deviceId } = await fetchPasswordlessStart({
+    phoneNumber: inputState.phoneNumber,
+    deviceUniqueId,
+    isDevice: Device.isDevice,
+    platform: Platform.OS,
+    brand: toString(Device.brand),
+    manufacturer: toString(Device.manufacturer),
+    model: toString(Device.modelName),
+    modelId: toString(Device.modelId),
+    designName: toString(Device.designName),
+    productName: toString(Device.productName),
+    deviceYearClass: toString(Device.deviceYearClass),
+    supportedCpuArch: toString(Device.supportedCpuArchitectures?.join(",")),
+    os: toString(Device.osName),
+    osVersion: toString(Device.osVersion),
+    osBuildId: toString(Device.osBuildId),
+    osInternalBuildId: toString(Device.osInternalBuildId),
+    androidApiLevel: toString(Device.platformApiLevel),
+    deviceName: toString(Device.deviceName),
+  });
+  if (error) {
+    // TODO: handle server errors in snack bar
+    inputState.setPhoneValidation([error]);
+    return;
+  }
+  if (!otpToken) {
+    logger({
+      container: "authnz",
+      path: { section: "usecases", file: "passwordlessStart" },
+      type: "error",
+      logMessage: "otp token is not defined by server",
+    });
+    return;
+  }
+  authState.setOtpToken(otpToken);
+  storage.add("device_id", deviceId);
 }
--- app/authentication/usecases/passwordlessVerify.ts
+++ app/authentication/usecases/passwordlessVerify.ts
@@ -1,13 +1,37 @@
-import { secureStorage, storage } from "../../library";
+import { secureStorage, toString, storage, logger } from "../../library";
 import { fetchPasswordlessVerify } from "../adapters";
 import { inputState, authState } from "../entities";
 
 export async function passwordlessVerify() {
   if (!inputState.otpNumber) {
     inputState.setOtpValidation(["کد تایید را وارد کنید"]);
+    return;
+  }
+  const deviceId = toString(storage.retrieve("device_id", "string"));
+  if (!deviceId) {
+    logger({
+      container: "authnz",
+      type: "error",
+      path: { section: "usecases", file: "passwordlessVerify" },
+      logMessage: "deviceId must be defined in this step, it's not defined",
+    });
+    return;
+  }
+  if (!authState.otpToken) {
+    logger({
+      container: "authnz",
+      type: "error",
+      path: { section: "usecases", file: "passwordlessVerify" },
+      logMessage: "otpToken must be defined in this step, it's not defined",
+    });
+    return;
   }
   const { error, jwt, jwtExpires, refreshExpires, refreshToken, role, userId } =
-    await fetchPasswordlessVerify(inputState.otpNumber);
+    await fetchPasswordlessVerify({
+      otpCode: inputState.otpNumber,
+      deviceId,
+      otpToken: authState.otpToken,
+    });
   if (error) {
     inputState.setOtpValidation([error]);
     inputState.setOtpNumber("");
@@ -21,20 +45,43 @@ export async function passwordlessVerify() {
     !role ||
     !userId
   ) {
+    logger({
+      container: "authnz",
+      type: "error",
+      path: { section: "usecases", file: "passwordlessVerify" },
+      logMessage: `jwt: ${typeof jwt}, refreshToken: ${typeof refreshToken}, refreshExpires: ${typeof refreshExpires},
+       jwtExpires: ${typeof jwtExpires}, role: ${typeof role}, userId: ${typeof userId}`,
+    });
     return;
   }
-  await Promise.all([
-    secureStorage.add("refresh_token", refreshToken),
-    secureStorage.add("token", jwt),
-  ]);
+  try {
+    await Promise.all([
+      secureStorage.add("refresh_token", refreshToken),
+      secureStorage.add("token", jwt),
+    ]);
+  } catch (error) {
+    logger({
+      container: "authnz",
+      type: "error",
+      path: { section: "usecases", file: "passwordlessVerify" },
+      logMessage: `secure storage add refresh and jwt error: ${error}`,
+    });
+    return;
+  }
+
   storage.add("token_expires_at", jwtExpires);
   storage.add("refresh_expires_at", refreshExpires);
   storage.add("role", role);
-  storage.add("userId", userId);
-  // console.log("passwordlessVerify", { role });
+  storage.add("user_id", userId);
   if (role === "customer" || role === "provider") {
     authState.setRole(role);
   } else {
+    logger({
+      container: "authnz",
+      type: "warn",
+      path: { section: "usecases", file: "passwordlessVerify" },
+      logMessage: `role not defined properly: ${role}`,
+    });
     authState.setRole("customer");
   }
   authState.setRefreshToken(refreshToken);
