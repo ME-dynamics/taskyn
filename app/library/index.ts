93dd66f3fe95d4658f5fef679ab1c3afd283092f
eyousefifar <eyousefifar@gmail.com>
2022-03-09 20:27:55 +0330

    export logger, app state, tPayload from lib

--- app/library/index.ts
+++ app/library/index.ts
@@ -4,7 +4,7 @@ export { THEME } from "./theme";
 export { Touchable } from "./Touchable";
 export { Caption, Headline, Paragraph, Subheading, Title } from "./Typography";
 export { SearchBar } from "./SearchBar";
-export { request, uploadFile } from "./network";
+export { request, uploadFile, tPayload } from "./network";
 export { secureStorage, storage } from "./storage";
 export { Tap } from "./Tap";
 export { openCamera, openCropper, openGallery } from "./cropPicker";
@@ -14,7 +14,7 @@ export { Scroller, tScrollerRef } from "./Scroller";
 export { IconButton } from "./IconButton";
 export { toJalaaliDate, parseDate } from "./toJalaaliDate";
 export { toString } from "./toString";
-
+export { logger } from "./logger";
 export { Checkbox } from "./CheckBox";
 export { Radio } from "./Radio";
 export { SelectButton } from "./SelectButton";
@@ -24,3 +24,8 @@ export { CustomBackdrop } from "./CustomBackdrop";
 export { Avatar } from "./Avatar";
 export { SuccessAlert } from "./SuccessAlert";
 export * from "./events";
+export {
+  getAppState,
+  registerAppState,
+  removeAppStateListeners,
+} from "./appState";
