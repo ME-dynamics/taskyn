053450114fe4ffea69e8196c26851c4d4fa0b939
eyousefifar <eyousefifar@gmail.com>
2022-03-11 16:33:05 +0330

    add logger to retiriveTestResult

--- app/psychologyTests/usecases/retrieveTestResult.ts
+++ app/psychologyTests/usecases/retrieveTestResult.ts
@@ -1,13 +1,29 @@
+import { logger } from "../../library";
 import { fetchGetTestResultById } from "../adapters";
 import { testResultState } from "../entities";
 
-export async function retrieveTestResult(testId: string) {
-  const { error, testResult } = await fetchGetTestResultById(testId);
+export async function retrieveTestResult(testId: string, customerId: string) {
+  const { error, testResult } = await fetchGetTestResultById(
+    testId,
+    customerId
+  );
   if (error) {
+    logger({
+      container: "psychologyTests",
+      path: { section: "usecases", file: "retrieveTestResult" },
+      type: "error",
+      logMessage: `Error retrieving test result: ${error}`,
+    });
     return {
       error,
       testResult: [],
     };
   }
+  logger({
+    container: "psychologyTests",
+    path: { section: "usecases", file: "retrieveTestResult" },
+    type: "info",
+    logMessage: `Test result retrieved for testId: ${testId}`,
+  });
   testResultState.setTestResult(testResult);
 }
