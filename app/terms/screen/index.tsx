e600492933ad56ecc0af66fa8cc752c23ae6011a
eyousefifar <eyousefifar@gmail.com>
2022-04-01 20:51:30 +0430

    implementation term screen

---
+++ app/terms/screen/index.tsx
@@ -0,0 +1,16 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { View } from "react-native";
+import { Logo, Paragraph, Scroller, Container } from "../../library";
+import { termsText } from "../constant";
+import { styles } from "./styles";
+function TermsScreen() {
+  return (
+    <Container>
+      <Scroller contentContainerStyle={styles.container}>
+        <Paragraph>{termsText}</Paragraph>
+      </Scroller>
+    </Container>
+  );
+}
+export const Terms = observer(TermsScreen);
