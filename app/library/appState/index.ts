9f653a768e98fe6df9c22c0c854f43c42156ba81
eyousefifar <eyousefifar@gmail.com>
2022-04-02 13:11:43 +0430

    remove app state loggers

--- app/library/appState/index.ts
+++ app/library/appState/index.ts
@@ -10,76 +10,76 @@ export function getAppState() {
 
 export function removeAppStateListeners() {
   rnAppState.removeEventListener("change", () => {
-    logger({
-      container: "app",
-      type: "info",
-      path: { section: "library", file: "appState/index" },
-      logMessage: "app state change listener removed",
-    });
+    // logger({
+    //   container: "app",
+    //   type: "info",
+    //   path: { section: "library", file: "appState/index" },
+    //   logMessage: "app state change listener removed",
+    // });
   });
   if (Platform.OS === "android") {
     rnAppState.removeEventListener("blur", () => {
-      logger({
-        container: "app",
-        type: "info",
-        path: { section: "library", file: "appState/index" },
-        logMessage: "app state blur listener removed",
-      });
+      // logger({
+      //   container: "app",
+      //   type: "info",
+      //   path: { section: "library", file: "appState/index" },
+      //   logMessage: "app state blur listener removed",
+      // });
     });
     rnAppState.removeEventListener("focus", () => {
-      logger({
-        container: "app",
-        type: "info",
-        path: { section: "library", file: "appState/index" },
-        logMessage: "app state focus listener removed",
-      });
+      // logger({
+      //   container: "app",
+      //   type: "info",
+      //   path: { section: "library", file: "appState/index" },
+      //   logMessage: "app state focus listener removed",
+      // });
     });
   }
   rnAppState.removeEventListener("memoryWarning", () => {
-    logger({
-      container: "app",
-      type: "info",
-      path: { section: "library", file: "appState/index" },
-      logMessage: "app state memory warning listener removed",
-    });
+    // logger({
+    //   container: "app",
+    //   type: "info",
+    //   path: { section: "library", file: "appState/index" },
+    //   logMessage: "app state memory warning listener removed",
+    // });
   });
 }
 
 export function registerAppState() {
   rnAppState.addEventListener("change", (event) => {
-    logger({
-      container: "app",
-      type: "info",
-      path: { section: "library", file: "appState/index" },
-      logMessage: "app state change, event: " + event,
-    });
+    // logger({
+    //   container: "app",
+    //   type: "info",
+    //   path: { section: "library", file: "appState/index" },
+    //   logMessage: "app state change, event: " + event,
+    // });
     appState.setStatus(event);
   });
   if (Platform.OS === "android") {
     rnAppState.addEventListener("blur", (event) => {
-      logger({
-        container: "app",
-        type: "info",
-        path: { section: "library", file: "appState/index" },
-        logMessage: "app state blur, event: " + event,
-      });
+      // logger({
+      //   container: "app",
+      //   type: "info",
+      //   path: { section: "library", file: "appState/index" },
+      //   logMessage: "app state blur, event: " + event,
+      // });
     });
     rnAppState.addEventListener("focus", (event) => {
-      logger({
-        container: "app",
-        type: "info",
-        path: { section: "library", file: "appState/index" },
-        logMessage: "app state focus, event: " + event,
-      });
+      // logger({
+      //   container: "app",
+      //   type: "info",
+      //   path: { section: "library", file: "appState/index" },
+      //   logMessage: "app state focus, event: " + event,
+      // });
     });
   }
 
   rnAppState.addEventListener("memoryWarning", (event) => {
-    logger({
-      container: "app",
-      type: "warn",
-      path: { section: "library", file: "appState/index" },
-      logMessage: "memory leak in app state, event: " + event,
-    });
+    // logger({
+    //   container: "app",
+    //   type: "warn",
+    //   path: { section: "library", file: "appState/index" },
+    //   logMessage: "memory leak in app state, event: " + event,
+    // });
   });
 }
