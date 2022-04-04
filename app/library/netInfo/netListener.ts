874cb878420c0577b88df420eb27e43c38c0ac4e
eyousefifar <eyousefifar@gmail.com>
2022-04-04 13:22:22 +0430

    state listener, update net info move statee listener function out of callback for reuse. updateNetInfo function using NetInfo fetch. this function is used in app state active to check for netinfo status. this fixes a bug where phone is put to sleep, network disconnects. and when phone wakes up, netInfo does not refresh. using this method is app state should fix this issuue

--- app/library/netInfo/netListener.ts
+++ app/library/netInfo/netListener.ts
@@ -1,4 +1,5 @@
-import NetInfo from "@react-native-community/netinfo";
+import NetInfo, { NetInfoState } from "@react-native-community/netinfo";
+import { logger } from "../logger";
 import { netInfoState } from "./netInfoState";
 // use net info
 // NetInfo.configure({
@@ -11,43 +12,59 @@ import { netInfoState } from "./netInfoState";
 //   shouldFetchWiFiSSID: false // met iOS requirements to get SSID. Will leak memory if set to true without meeting requirements.
 // });
 
+function stateListener(state: NetInfoState) {
+  const { isConnected, isInternetReachable } = state;
+  netInfoState.setConnection(!!isConnected);
+  netInfoState.setInternet(!!isInternetReachable);
+  if (state.type === "wifi") {
+    const {
+      bssid,
+      frequency,
+      ipAddress,
+      isConnectionExpensive,
+      ssid,
+      strength,
+      subnet,
+    } = state.details;
+    // write to database;
+  }
+  if (state.type === "cellular") {
+    const { carrier, cellularGeneration, isConnectionExpensive } =
+      state.details;
+  }
+  if (state.type === "vpn") {
+    netInfoState.setVpn(true);
+  }
+  if (state.type !== "vpn") {
+    netInfoState.setVpn(false);
+  }
+  if (
+    state.type === "bluetooth" ||
+    state.type === "ethernet" ||
+    state.type === "wimax" ||
+    state.type === "other"
+  ) {
+    // save state type
+    // write to database
+    const { isConnectionExpensive } = state.details;
+  }
+}
+
 export function registerNetInfo() {
-  const unsubscribe = NetInfo.addEventListener(function stateListener(state) {
-    const { isConnected, isInternetReachable } = state;
-    netInfoState.setConnection(!!isConnected);
-    netInfoState.setInternet(!!isInternetReachable);
-    if (state.type === "wifi") {
-      const {
-        bssid,
-        frequency,
-        ipAddress,
-        isConnectionExpensive,
-        ssid,
-        strength,
-        subnet,
-      } = state.details;
-      // write to database;
-    }
-    if (state.type === "cellular") {
-      const { carrier, cellularGeneration, isConnectionExpensive } =
-        state.details;
-    }
-    if (state.type === "vpn") {
-      netInfoState.setVpn(true);
-    }
-    if (state.type !== "vpn") {
-      netInfoState.setVpn(false);
-    }
-    if (
-      state.type === "bluetooth" ||
-      state.type === "ethernet" ||
-      state.type === "wimax" ||
-      state.type === "other"
-    ) {
-      // save state type
-      // write to database
-      const { isConnectionExpensive } = state.details;
-    }
-  });
+  const unsubscribe = NetInfo.addEventListener(stateListener);
   return unsubscribe;
+}
+
+export async function updateNetInfo() {
+  try {
+    const state = await NetInfo.fetch();
+    stateListener(state);
+  } catch (error) {
+    logger({
+      container: "app",
+      path: { section: "library", file: "netInfo/netListener.ts" },
+      type: "error",
+      logMessage: `Error in updateNetInfo: ${error}`,
+    });
+  }
 }
