4841dda32889277fcd3fa82b4e8fe1aa88679090
eyousefifar <eyousefifar@gmail.com>
2022-04-05 20:07:59 +0430

    add userId to usecase in task

--- app/task/usecases/taskDone.ts
+++ app/task/usecases/taskDone.ts
@@ -1,8 +1,8 @@
 import { fetchTaskDone } from "../adapters";
 import { taskState } from "../entities";
 
-export async function taskDone(taskId: string) {
-  const done = await fetchTaskDone(taskId);
+export async function taskDone(taskId: string, userId: string) {
+  const done = await fetchTaskDone(taskId, userId);
   if (done) {
     taskState.setTaskDone(taskId);
   } else {
